7123c4ef3a97c22401b15d33e5b1e914b0530ad3
Jessi Berkelhammer <jessib@riseup.net>
2007-04-11 14:07:54 +0000

    added migration to create tasklist

---
+++ db/migrate/034_create_tasklist.rb
@@ -0,0 +1,23 @@
+class CreateTasklist < ActiveRecord::Migration
+  def self.up
+    create_table :task_lists do |t|
+    end
+    create_table :tasks do |t|
+      t.column :task_list_id, :integer
+      t.column :name, :string, :limit => 50
+      t.column :description, :string
+      t.column :completed, :boolean, :default => false
+    end
+    create_table :tasks_users, :id => false do |t|
+      t.column :user_id, :integer
+      t.column :task_id, :integer
+    end
+  end 
+
+
+  def self.down
+    drop_table :task_lists
+    drop_table :tasks
+    drop_table :tasks_users
+  end
+end
