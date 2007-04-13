a058cb035eda8db3bdcbaf04a4ae09e5a70f3bfe
Elijah Saxon <elijah@riseup.net>
2007-04-13 11:50:40 +0000

    added style and homepage to groups

---
+++ db/migrate/032_add_home_to_groups.rb
@@ -0,0 +1,13 @@
+class AddHomeToGroups < ActiveRecord::Migration
+  def self.up
+    add_column :groups, :private_home_id, :integer
+    add_column :groups, :public_home_id, :integer
+    add_column :groups, :style, :string
+  end
+
+  def self.down
+    remove_column :groups, :private_home_id
+    remove_column :groups, :public_home_id
+    #remove_column :groups, :style
+  end
+end
