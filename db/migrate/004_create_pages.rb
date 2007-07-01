93b0de31a572f5dad4bd55c2862f44334d05de77
Elijah Saxon <elijah@riseup.net>
2007-07-01 23:19:54 +0000

    indent correction

--- db/migrate/004_create_pages.rb
+++ db/migrate/004_create_pages.rb
@@ -2,20 +2,20 @@ class CreatePages < ActiveRecord::Migration
   def self.up
     create_table :pages do |t|
       t.column :title, :string
-	  t.column :created_at, :datetime
-	  t.column :updated_at, :datetime
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
       t.column :happens_at, :datetime
-	  t.column :resolved,   :boolean, :default => true
-	  t.column :public,     :boolean
+      t.column :resolved,   :boolean, :default => true
+      t.column :public,     :boolean
       t.column :created_by_id,   :integer
-	  t.column :updated_by_id,   :integer
-	  t.column :summary,         :string
-	  t.column :type,            :string
+      t.column :updated_by_id,   :integer
+      t.column :summary,         :string
+      t.column :type,            :string
       t.column :message_count, :integer, :default => 0
 
-	  # polymorphic association
-	  t.column :data_id, :integer
-	  t.column :data_type, :string
+      # polymorphic association
+      t.column :data_id, :integer
+      t.column :data_type, :string
     end
   end
 
