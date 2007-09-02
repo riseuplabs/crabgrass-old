9e2972413bc589d5736a85b78f8bdef2445977d1
Elijah Saxon <elijah@riseup.net>
2007-09-02 07:25:13 +0000

    code cleanup: reorganized page.rb

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,23 +1,3 @@
-# == Schema Information
-# Schema version: 24
-#
-# Table name: pages
-#
-#  id              :integer(11)   not null, primary key
-#  title           :string(255)   
-#  created_at      :datetime      
-#  updated_at      :datetime      
-#  happens_at      :datetime      
-#  resolved        :boolean(1)    
-#  public          :boolean(1)    
-#  needs_attention :boolean(1)    
-#  created_by_id   :integer(11)   
-#  updated_by_id   :integer(11)   
-#  summary         :string(255)   
-#  controller      :string(255)   
-#  tool_id         :integer(11)   
-#  tool_type       :string(255)   
-#
 
 # notes
 # all the relationship between a page and its groups is stored in the group_participations
@@ -29,19 +9,87 @@
 class Page < ActiveRecord::Base
   acts_as_modified
   acts_as_taggable
-  #acts_as_ferret :additional_fields => []
 
-  # to be set by subclasses (ie tools)
-  class_attribute :controller, :model, :icon, :internal?,
-    :class_description, :class_display_name, :class_group
+  #######################################################################
+  ## PAGE NAMING
+  
+  validates_format_of  :name, :with => /^$|^[a-z0-9]+([-_]*[a-z0-9]+){1,39}$/
 
-  ### associations ###  
+  def validate
+    if name_modified? and name_taken?
+      errors.add 'name', 'is already taken'
+    end
+  end
+
+  def name_url
+    name.any? ? name : friendly_url
+  end
+  
+  def friendly_url
+    s = title.nameize
+    s = s[0..40].sub(/-([^-])*$/,'') if s.length > 42     # limit name length, and remove any half-cut trailing word
+    "#{s}+#{id}"
+  end
+
+  # returns true if self's unique page name is already in use.
+  # what pages are in the namespace? all pages connected to all
+  # groups connected to this page (include the group's committees too).
+  def name_taken?
+    return false unless self.name.any?
+    p = Page.find(:first,
+      :conditions => ['pages.name = ? and group_participations.group_id IN (?)', self.name, self.namespace_group_ids],
+      :include => :group_participations
+    )
+    return false if p.nil?
+    return self != p
+  end
+
+  #######################################################################
+  ## RELATIONSHIP TO PAGE DATA
   
   belongs_to :data, :polymorphic => true
   has_one :discussion, :dependent => :destroy
   has_many :assets, :dependent => :destroy
+      
+  validates_presence_of :title
+  validates_associated :data
+
+  def unresolve
+    resolve(false)
+  end
+  def resolve(value=true)
+    user_participations.each do |up|
+      up.resolved = value
+      up.save
+    end
+    resolved = value
+    save
+  end  
+
+  def build_post(post,user)
+    # this looks like overkill, but it seems to be needed
+    # in order to build the post in memory and have it saved when
+    # (possibly new) pages is saved
+    self.discussion ||= Discussion.new
+    self.discussion.page = self
+    if post.instance_of? String
+      post = Post.new(:body => post)
+    end
+    self.discussion.posts << post
+    post.discussion = self.discussion
+    post.user = user
+    return post
+  end
   
-  # relationship of this page to users
+  ## update ASSET permissions
+  after_save :update_access
+  def update_access
+    assets.each { |asset| asset.update_access }
+  end
+  
+  #######################################################################
+  ## RELATIONSHIP TO USERS
+
   belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by_id'
   belongs_to :updated_by, :class_name => 'User', :foreign_key => 'updated_by_id'
   has_many :user_participations, :dependent => :destroy
@@ -54,11 +102,6 @@ class Page < ActiveRecord::Base
     end
   end
 
-  # relationship of this page to groups
-  has_many :group_participations, :dependent => :destroy
-  has_many :groups, :through => :group_participations
-  belongs_to :group # the main group
-  
   # like users.with_access, but uses already included data
   def users_with_access
     user_participations.collect{|part| part.user if part.access }.compact
@@ -74,14 +117,62 @@ class Page < ActiveRecord::Base
     user_participations.detect{|p| p.user_id==user.id }
   end
 
+  # used for ferret index
+  def user_ids
+    user_participations.collect{|upart|upart.user_id}
+  end
+
+  before_create :set_user
+  def set_user
+    if User.current
+      self.created_by = User.current
+      self.created_by_login = self.created_by.login
+      self.updated_by       = self.created_by
+      self.updated_by_login = self.created_by.login
+    end
+    true
+  end
+
+  #######################################################################
+  ## RELATIONSHIP TO GROUPS
+  
+  has_many :group_participations, :dependent => :destroy
+  has_many :groups, :through => :group_participations
+  belongs_to :group # the main group
+  
+  has_many :namespace_groups, :class_name => 'Group', :finder_sql => 'SELECT groups.* FROM groups WHERE groups.id IN (#{namespace_group_ids_sql})'
+  
+  # When getting a list of ids of groups for this page,
+  # we use group_participations. This way, we will have
+  # current data even if a group is added and the page
+  # has not yet been saved.
+  # used extensively, and by ferret.
+  def group_ids
+    group_participations.collect{|gpart|gpart.group_id}
+  end
+  
+  # returns an array of group ids that compose this page's namespace
+  # includes direct groups and all the relatives of the direct groups.
+  def namespace_group_ids
+    Group.namespace_ids(group_ids)
+  end
+  def namespace_group_ids_sql
+    namespace_group_ids.any? ? namespace_group_ids.join(',') : 'NULL'
+  end
+
   # takes an array of group ids, return all the matching group participations
   # this is called a lot, since it is used to determine permission for the page
   def participation_for_groups(group_ids) 
-    group_participations.collect{|gpart| gpart if group_ids.include? gpart.group_id }.compact
+    group_participations.collect do |gpart|
+      gpart if group_ids.include? gpart.group_id
+    end.compact
   end
   def participation_for_group(group)
     group_participations.detect{|gpart| gpart.group_id == group.id}
   end
+
+  #######################################################################
+  ## RELATIONSHIP TO OTHER PAGES
   
   # reciprocal links between pages
   has_and_belongs_to_many :links,
@@ -101,83 +192,10 @@ class Page < ActiveRecord::Base
   def add_link(page)
     links << page unless links.include?(page)
   end
-  
-  ### validations ###
-  
-  validates_presence_of :title
-  validates_associated :data
-
-  # page name must start with a letter.
-  validates_format_of  :name, :with => /^$|^[a-z]+([-_]*[a-z0-9]+){1,39}$/
-
-  def validate
-    if name_modified? and name_taken?
-      errors.add 'name', 'is already taken'
-    end
-  end
-    
-  ### accessors ###
-  
-  def name_url
-    name.any? ? name : friendly_url
-  end
-  
-  def friendly_url
-    s = title.nameize
-    s = s[0..40].sub(/-([^-])*$/,'') if s.length > 42     # limit name length, and remove any half-cut trailing word
-    "#{s}+#{id}"
-  end
-  
-  # lets us convert from a url pretty name to the actual class.
-  def self.display_name_to_class(display_name)
-    dn = display_name.nameize
-    TOOLS.detect{|t|t.class_display_name.nameize == dn if t.class_display_name}
-  end 
-  # return an array of page classes that are members of class_group
-  def self.class_group_to_class_names(class_group)
-    TOOLS.collect{|t|t.to_s if t.class_group == class_group and t.class_group}.compact
-  end 
-  
-  ### callbacks ###
-
-  before_create :set_user, :single_table_inheritance_hack
-  def set_user
-    if User.current
-      self.created_by = User.current
-      self.created_by_login = self.created_by.login
-      self.updated_by       = self.created_by
-      self.updated_by_login = self.created_by.login
-    end
-    true
-  end
-  def single_table_inheritance_hack
-    self.type = self.class.to_s
-    # ^^^^^ to work around bug in rails with namespaced
-    # models. see http://dev.rubyonrails.org/ticket/7630
-  end
+   
  
-  before_save :denormalize
-  def denormalize
-    # denormalize hack follows:
-    if changed? :groups 
-      # we use group_participations because self.groups might not
-      # reflect current data if unsaved.
-      group = (group_participations.first.group if group_participations.any?)
-      self.group_name = (group.name if group)
-      self.group_id = (group.id if group)
-    end
-    if changed? :updated_by
-      self.updated_by_login = updated_by.login
-    end
-    true
-  end
-
-  after_save :update_access
-  def update_access
-    assets.each { |asset| asset.update_access }
-  end
-  
-  ### association modifiers ##################################################
+  #######################################################################
+  ## RELATIONSHIP TO ENTITIES
     
   # add a group or user participation to this page
   def add(entity, attributes={})
@@ -203,71 +221,50 @@ class Page < ActiveRecord::Base
     end
   end
 
-  def build_post(post,user)
-    # this looks like overkill, but it seems to be needed
-    # in order to build the post in memory and have it saved when
-    # (possibly new) pages is saved
-    self.discussion ||= Discussion.new
-    self.discussion.page = self
-    if post.instance_of? String
-      post = Post.new(:body => post)
+  
+  #######################################################################
+  ## SUPPORT FOR PAGE SUBCLASSING
+
+  # to be set by subclasses (ie tools)
+  class_attribute :controller, :model, :icon, :internal?,
+    :class_description, :class_display_name, :class_group
+
+  # lets us convert from a url pretty name to the actual class.
+  def self.display_name_to_class(display_name)
+    dn = display_name.nameize
+    TOOLS.detect{|t|t.class_display_name.nameize == dn if t.class_display_name}
+  end 
+  # return an array of page classes that are members of class_group
+  def self.class_group_to_class_names(class_group)
+    TOOLS.collect{|t|t.to_s if t.class_group == class_group and t.class_group}.compact
+  end 
+
+  before_create :single_table_inheritance_hack
+  def single_table_inheritance_hack
+    self.type = self.class.to_s
+    # ^^^^^ to work around bug in rails with namespaced
+    # models. see http://dev.rubyonrails.org/ticket/7630
+  end
+
+  #######################################################################
+  ## DENORMALIZATION
+
+  before_save :denormalize
+  def denormalize
+    # denormalize hack follows:
+    if changed? :groups 
+      # we use group_participations because self.groups might not
+      # reflect current data if unsaved.
+      group = (group_participations.first.group if group_participations.any?)
+      self.group_name = (group.name if group)
+      self.group_id = (group.id if group)
     end
-    self.discussion.posts << post
-    post.discussion = self.discussion
-    post.user = user
-    return post
-  end
-    
-  def unresolve
-    resolve(false)
-  end
-  def resolve(value=true)
-    user_participations.each do |up|
-      up.resolved = value
-      up.save
+    if changed? :updated_by
+      self.updated_by_login = updated_by.login
     end
-    resolved = value
-    save
+    true
   end
   
-  def self.make(function,options={})
-    PageStork.send(function, options)
-  end
-
-
-  # When getting a list of ids of groups for this page,
-  # we use group_participations. This way, we will have
-  # current data even if a group is added and the page
-  # has not yet been saved.
-  # used extensively, and by ferret.
-  def group_ids
-    group_participations.collect{|gpart|gpart.group_id}
-  end
-  
-  # used for ferret index
-  def user_ids
-    user_participations.collect{|upart|upart.user_id}
-  end
-  
-  # returns true if self's unique page name is already in use.
-  # what pages are in the namespace? all pages connected to all
-  # groups connected to this page (include the group's committees too).
-  def name_taken?
-    return false unless self.name.any?
-    p = Page.find(:first,
-      :conditions => ['pages.name = ? and group_participations.group_id IN (?)', self.name, self.namespace_group_ids],
-      :include => :group_participations
-    )
-    return false if p.nil?
-    return self != p
-  end
-
-  # returns an array of group ids that compose this page's namespace
-  # includes direct groups and all the relatives of the direct groups.
-  def namespace_group_ids
-    Group.namespace_ids(group_ids)
-  end
-
   # used to mark stuff that has been changed.
   # so that we know we need to update other stuff when saving.
   def changed(what)
@@ -278,9 +275,17 @@ class Page < ActiveRecord::Base
     @changed ||= {}
     @changed[what]
   end
-  
+
+  #######################################################################
+  ## MISC. HELPERS
+
   # tmp in-memory storage used by views
   def flag
     @flags ||= {}
   end
+
+  def self.make(function,options={})
+    PageStork.send(function, options)
+  end
+
 end
