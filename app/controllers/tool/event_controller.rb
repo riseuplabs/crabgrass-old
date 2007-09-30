5efaa46455f05bba16ed2c034371d06a2476e98f
Elijah Saxon <elijah@riseup.net>
2007-09-30 06:59:42 +0000

    changed tabs to spaces

--- app/controllers/tool/event_controller.rb
+++ app/controllers/tool/event_controller.rb
@@ -23,47 +23,47 @@ class Tool::EventController < Tool::BaseController
   end
 
   def update
-	  @page.attributes = params[:page]
-	  @event.attributes = params[:event]
-	  if @page.save and @event.save
-		  return redirect_to page_url(@page)
-	  else
-		  message :object => @page
-	  end
+    @page.attributes = params[:page]
+    @event.attributes = params[:event]
+    if @page.save and @event.save
+      return redirect_to page_url(@page)
+    else
+      message :object => @page
+    end
   end
 
 
   def create
-   @page_class = Tool::Event
-   @event = ::Event.new   
+    @page_class = Tool::Event
+    @event = ::Event.new   
     if request.post?
-	    @page = build_new_page @page_class
+      @page = build_new_page @page_class
 
-	    d = params[:date_start].split("/")
-	    params[:date_start] = [d[1], d[0], d[2]].join("/")
-	    params[:time_start] =  params[:date_start] + " "+ params[:hour_start]
+      d = params[:date_start].split("/")
+      params[:date_start] = [d[1], d[0], d[2]].join("/")
+      params[:time_start] =  params[:date_start] + " "+ params[:hour_start]
 
-	    @page.starts_at = TzTime.zone.local_to_utc(params[:time_start].to_time)
+      @page.starts_at = TzTime.zone.local_to_utc(params[:time_start].to_time)
 
-	    d = params[:date_end].split("/")
-	    params[:date_end] = [d[1], d[0], d[2]].join("/")
+      d = params[:date_end].split("/")
+      params[:date_end] = [d[1], d[0], d[2]].join("/")
 
-	    params[:time_end] =  params[:date_end] + " " + params[:hour_end]
-	    @page.ends_at = TzTime.zone.local_to_utc(params[:time_end].to_time)
-	    @event = ::Event.new params[:event]
-	    @page.data = @event
-	    if @page.save
-		   return redirect_to page_url(@page)
-	    else
-		    message :object => @page
-	    end
+      params[:time_end] =  params[:date_end] + " " + params[:hour_end]
+      @page.ends_at = TzTime.zone.local_to_utc(params[:time_end].to_time)
+      @event = ::Event.new params[:event]
+      @page.data = @event
+      if @page.save
+        return redirect_to page_url(@page)
+      else
+        message :object => @page
+      end
     end
   end
  
  def set_event_description
-	 @event.description =  params[:value]
-	 @event.save
-	 render :text => @event.description_html
+   @event.description =  params[:value]
+   @event.save
+   render :text => @event.description_html
  end
 
  def participate
@@ -72,15 +72,15 @@ class Tool::EventController < Tool::BaseController
      @user_participation.watch = params[:user_participation_watch]
      @user_participation.attend = false
    else
-	   if !params[:user_participation_attend].nil?
-	       @user_participation.watch = false
-	       @user_participation.attend = params[:user_participation_attend]
-	   else
-		@user_participation.watch = false
-		@user_participation.attend = false
-		# remove the user participation from the table?
-	   end
-    end
+     if !params[:user_participation_attend].nil?
+       @user_participation.watch = false
+       @user_participation.attend = params[:user_participation_attend]
+     else
+       @user_participation.watch = false
+       @user_participation.attend = false
+       # remove the user participation from the table?
+     end
+   end
 
    @user_participation.save
 
@@ -103,14 +103,14 @@ class Tool::EventController < Tool::BaseController
   
   # set the right time format for the event
   def set_time (time)
-	time
+    time
   end
 
   def authorized?
-	  if params[:action] == 'set_event_description' or params[:action] == 'edit'
-		  return current_user.may?(:admin, @page)
-	  else
-		  return true
-	  end
+    if params[:action] == 'set_event_description' or params[:action] == 'edit'
+      return current_user.may?(:admin, @page)
+    else
+      return true
+    end
   end
 end
