10e52c87d23900b23e3760cea856966d43d61c25
Seth Walker <seth@radicaldesigns.org>
2007-12-04 07:47:48 +0000

    make thumbnails for asset versions

---
+++ db/migrate/1196752821_real_asset_versions.rb
@@ -0,0 +1,17 @@
+class RealAssetVersions < ActiveRecord::Migration
+  def self.up
+    Asset::Version.find(:all, :conditions => "parent_id IS NULL").each do |version|
+      filename = if version.asset.version == version.version
+                   version.asset.full_filename
+                 else
+                   version.full_filename
+                 end
+      Asset::Version.attachment_options[:thumbnails].each do |suffix, size|
+        version.create_or_update_thumbnail(filename, suffix, *size)
+      end
+    end
+  end
+
+  def self.down
+  end
+end
