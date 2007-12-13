eff4eb81200c8a5c3235ebf95f8e062a8651dfe2
Seth Walker <seth@radicaldesigns.org>
2007-12-13 03:04:11 +0000

    accurate schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 61) do
+ActiveRecord::Schema.define(:version => 1196752821) do
 
   create_table "asset_versions", :force => true do |t|
     t.column "asset_id",       :integer
@@ -92,17 +92,6 @@ ActiveRecord::Schema.define(:version => 61) do
   end
 
   add_index "email_addresses", ["profile_id"], :name => "email_addresses_profile_id_index"
-
-  create_table "event_recurrencies", :force => true do |t|
-    t.column "event_id",          :integer
-    t.column "start",             :datetime
-    t.column "end",               :datetime
-    t.column "type",              :string
-    t.column "day_of_the_week",   :string
-    t.column "day_of_the_month",  :string
-    t.column "month_of_the_year", :string
-    t.column "created_at",        :datetime, :null => false
-  end
 
   create_table "events", :force => true do |t|
     t.column "description",      :text
