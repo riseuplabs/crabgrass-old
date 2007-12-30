f054f1151659b5e6e35d8983c6739988ccb0051c
Abie Flaxman <abie@riseup.net>
2007-12-30 08:05:19 +0000

    chat now has a date, as well as time, and the index of chat rooms is slightly better formated.

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,7 +1,7 @@
 module ChatHelper
 
   def message_content(message)
-    %(<div class="message #{message.level}" id="message-#{message.id}"><span class="time">#{message.created_at.strftime('%R')}</span> <span class="sender">#{message.sender_name}</span> <span class="content">#{message.content}</span></div>)
+    %(<div class="message #{message.level}" id="message-#{message.id}"><span class="time">#{message.created_at.strftime('%Y.%m.%d %R')}</span> <span class="sender">#{message.sender_name}</span> <span class="content">#{message.content}</span></div>)
   end
 
 end
