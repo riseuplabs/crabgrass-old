fb89097d41cb1d874f0c4d9559e39b70c4e80c7a
Seth Walker <seth@radicaldesigns.org>
2008-02-14 02:45:57 +0000

    don't autoload has_many_polymorphs

--- vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs.rb
+++ vendor/plugins/has_many_polymorphs/lib/has_many_polymorphs.rb
@@ -21,7 +21,7 @@ end
 if defined? Rails and RAILS_ENV and RAILS_ROOT
   _logger_warn "rails environment detected"
   require 'has_many_polymorphs/configuration'
-  require 'has_many_polymorphs/autoload'
+#  require 'has_many_polymorphs/autoload'
 end
 
 _logger_debug "loaded ok"
