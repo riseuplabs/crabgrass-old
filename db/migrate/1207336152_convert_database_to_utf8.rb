3685eb74248bc12a8567ae9ff072056effbb9d85
Elijah Saxon <elijah@riseup.net>
2008-04-04 19:16:49 +0000

    Convert the mysql database to use utf8 for its character set and collation. This will allow us to store arabic and cyrillic text

---
+++ db/migrate/1207336152_convert_database_to_utf8.rb
@@ -0,0 +1,17 @@
+class ConvertTablesToUnicode < ActiveRecord::Migration
+  def self.up
+    charset = 'utf8'
+    collation = 'utf8_general_ci'
+    execute "ALTER DATABASE #{connection.current_database} CHARACTER SET #{charset} COLLATE #{collation}"
+    connection.tables.each do |table|
+      execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET #{charset} COLLATE #{collation}"
+    end
+  end
+  def self.down
+    raise ActiveRecord::IrreversibleMigration.new()
+  end
+  def self.connection
+    ActiveRecord::Base.connection
+  end
+end
+
