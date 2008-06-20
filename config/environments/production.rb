72f545a7b12646b97e9fe9949082324a20eef56a
Elijah Saxon <elijah@riseup.net>
2008-06-20 08:36:47 +0000

    updated environments files to match rails 2.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,17 +4,21 @@
 # Code is not reloaded between requests
 config.cache_classes = true
 
-# do not log passwords to the log file.
-config.log_level = :warn
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = true
+config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors if you bad email addresses should just be ignored
+# Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
 begin
