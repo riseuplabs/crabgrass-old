44d727aee51d9754f284f27dee8708ef2642b221
Elijah Saxon <elijah@riseup.net>
2008-06-22 05:58:55 +0000

    (requires rake 8.1) installed engines plugin. if you are not running rake 8.1, do this: apt-get remove rake; gem install rake; ln -s /var/lib/gems/1.8/bin/rake /usr/local/bin

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
 
 #### ENUMERATIONS ##############
 
