a8447b0fbe5660fc9cfdd9fb4ef991530c49d505
Elijah Saxon <elijah@riseup.net>
2008-06-22 23:28:46 +0000

    remove disabled plugins

--- vendor/disabled/formy/init.rb
+++
@@ -1,6 +0,0 @@
-require 'formy'
-
-module ApplicationHelper
-  include Formy
-  Formy::define_formy_keywords
-end
--- vendor/disabled/formy/tasks/formy_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :formy do
-#   # Task goes here
-# end
