5981c972cbba3a9b50579cfdf07a815adc92c6c5
abie <abie@riseup>
2008-07-24 14:02:34 -0700

    Cleanup sloppy migration, so that tasks_users table is dropped after the data is moved to the task_participations table.  (And so that the down migration does the inverse action appropriately, as well)

--- db/migrate/20080723045752_create_task_participations_and_copy_data_from_task_users.rb
+++ db/migrate/20080723045752_create_task_participations_and_copy_data_from_task_users.rb
@@ -7,19 +7,31 @@ class CreateTaskParticipationsAndCopyDataFromTaskUsers < ActiveRecord::Migration
       
       t.references :user
       t.references :task
-
     end
 
     ActiveRecord::Base.record_timestamps = false
     TaskParticipation.reset_column_information
-    TasksUser.find(:all).each_with_index do |tuser, i|
+    TasksUser.find(:all).each do |tuser|
       tpart = TaskParticipation.create( :assigned => true, :user => tuser.user, :task => tuser.task )
       tpart.save!
     end
     
+    drop_table :tasks_users
   end
 
   def self.down
+    create_table :tasks_users do |t|
+      t.references :user
+      t.references :task
+   end
+
+    ActiveRecord::Base.record_timestamps = false
+    TasksUser.reset_column_information
+    TaskParticipation.find(:all).each do |tpart|
+      tuser = TasksUser.create( :user => tpart.user, :task => tpart.task )
+      tuser.save!
+    end
+
     drop_table :task_participations
   end
 end
