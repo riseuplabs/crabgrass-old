68f56b8760da91b9395011f2e50d2c96d6d21ac7
Elijah Saxon <elijah@riseup.net>
2008-08-24 17:56:11 -0700

    bugfix: only reindex page when tasklist really exists

--- tools/task_list_tool/app/models/task_list_page.rb
+++ tools/task_list_tool/app/models/task_list_page.rb
@@ -4,7 +4,7 @@ class TaskListPage < Page
 
   # Return string of all tasks, for the full text search index
   def index_data
-    if data.tasks
+    if data and data.tasks
       data.tasks.collect { |task| "#{task.name}\t#{task.description}" }.join "\n"
     end
   end
