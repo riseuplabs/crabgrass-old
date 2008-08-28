5d57a9a90a3193efa94ae231f7acbd543e5b552d
Pietro Ferrari <pietro@riseup.net>
2008-08-28 11:53:22 -0300

    Crabgrass UI is written in English, so English is our default. Respect users privacy and don't use google translation by default.

--- mods/gibberize/app/helpers/gibberize/base_helper.rb
+++ mods/gibberize/app/helpers/gibberize/base_helper.rb
@@ -3,9 +3,16 @@ module Gibberize::BaseHelper
     "<div class='errorExplanation'>#{flash[:notice]}</div>" if flash[:notice]
   end
 
-  # the application controller sets @site in a before_filter
-  def default_language
+  # this is set on RAILS_ROOT/config/sites.yml
+  def site_default_language
+    # the application controller sets @site in a before_filter
     Language.find_by_name(@site.default_language)
   end
+
+  # Crabgrass UI is written in English
+  def crabgrass_default_language
+    Language.find_by_name("English")
+  end
+
 end
 
