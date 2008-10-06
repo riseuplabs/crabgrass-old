fdcc4a409aac1cd9648e8b2237bfd8e23eafe306
Elijah Saxon <elijah@riseup.net>
2008-10-06 01:30:40 -0700

    added a lot of notes to page_terms.rb

--- app/models/page_terms.rb
+++ app/models/page_terms.rb
@@ -1,3 +1,44 @@
+=begin
+
+Some notes on PageTerms
+-----------------------
+
+See PageExtension::Index for most the code dealing with PageTerms.
+
+PageTerms holds searching information for a page. Every Page has exactly one
+PageTerms object and vice-versa. 
+
+PageTerms has three uses:
+
+(1) Sphinx full-text searching
+
+In order to index for sphinx, we need to be able to run a sql query without ruby
+logic. The page_terms table holds a copy of all the stuff we want to index for a
+particular page. It turns out that this is 100 times faster to keep a copy of
+this stuff in one table than trying to do complex queries on a sphinx reindex.
+
+(2) Page permissions
+
+PageTerms is also used to filter by access restrictions in non-sphinx page
+queries. We could just use sphinx for all queries, but the problem with this is
+that there are many situations in which the sphinx index could be out of date. 
+
+The page_terms table is a MyISAM table that has a fulltext index on
+(access_ids, tag_ids). It must be MyISAM for fulltext index. It is important
+to note that the fulltext index is a compound index and will ONLY work if BOTH
+columns are matched against in the query. This is very important!
+
+(3) Permissions for stuff that inherits page permissions
+
+Sometimes you want to search directly on a thing, without having to go through
+Page to get at the thing. For example, search directly for assets or tasks. But
+you also only want to return results for pages that the user has access to. In
+these cases, these tables hold a reference to page_terms and then the query just
+joins in the page_terms table and does a match against (access_ids, tag_ids) like
+in a normal page query.
+
+=end
+
 class PageTerms < ActiveRecord::Base
   belongs_to :page
 
@@ -51,8 +92,8 @@ class PageTerms < ActiveRecord::Base
   end
 
   # returns a string suitable for using in a fulltext match against
-  # page_terms.access_ids. The args are any number of users or groups.
-  # the filter will require all args have access.
+  # page_terms.access_ids. The args are any number of users or groups or :public.
+  # the filter will require that all args match.
   def self.access_filter_for(*args)
     filter_str = ""
     args.each do |arg|
