89ddde28723f665a317ff212fc7f2a81f186afe9
Chris <chris@brueckenschlaeger.de>
2008-10-16 02:14:39 +0200

    l10n on groups

--- app/models/group_extension/featured.rb
+++ app/models/group_extension/featured.rb
@@ -97,11 +97,12 @@ module GroupExtension
         private
         def raise_if_not_static
           if self.static != true
-            raise ArgumentError.new("Page is not static")
+            raise ArgumentError.new("Page is not static"[:page_is_not_static])
           end
         end
       end
-
+      
+      
     end
   end
 end
