b6f6b363898addbb687db36f9a1228d8f5466ae0
Elijah Saxon <elijah@riseup.net>
2008-11-09 20:21:46 -0800

    fixed group/tags, person/show

--- app/helpers/person_helper.rb
+++ app/helpers/person_helper.rb
@@ -1,12 +1,20 @@
 module PersonHelper
 
   def friend_link
-    if logged_in?
+    if logged_in? and current_user.id != @user.id
       if @user.friend_of?(current_user)
-        link_to "Remove from my contacts"[:remove_friend_link], {:controller => 'contact', :action => 'remove', :id => @user}
+        link = link_to "Remove from my contacts"[:remove_friend_link], {:controller => 'contact', :action => 'remove', :id => @user}
+         content_tag :li, link, :class => 'small_icon user_delete_16'
       elsif @user.profiles.visible_by(current_user).may_request_contact?
-        link_to "Add to my contacts"[:request_friend_link], {:controller => 'contact', :action => 'add', :id => @user}
+        link = link_to "Add to my contacts"[:request_friend_link], {:controller => 'contact', :action => 'add', :id => @user}
+        content_tag :li, link, :class => 'small_icon user_add_16'
       end
+    end
+  end
+
+  def edit_profile_link
+    if logged_in? and current_user.id == @user.id
+      content_tag :li, link_to("Edit Profile"[:edit_profile_link], :controller => 'profile', :action => 'edit', :id => 'public'), :class => 'small_icon pencil_16'
     end
   end
 
