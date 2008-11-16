57ae2c84d4e8cd04ab39509e07f0c95d2540fe8b
mike castleman <m@mlcastle.net>
2008-11-16 16:13:05 -0500

    l10n fixes (re: #289)

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -9,7 +9,8 @@ class GroupController < ApplicationController
   prepend_before_filter :find_group
   
   before_filter :login_required, :except => [:show, :archive, :tags, :search]
-    
+  before_filter :render_sidebar
+  
   verify :method => :post, :only => [:destroy, :update]
 
   def initialize(options={})
@@ -271,8 +272,11 @@ class GroupController < ApplicationController
   
   def find_group
     @group = Group.find_by_name params[:id] if params[:id]
-    @left_column = render_to_string(:partial => 'sidebar') if @group
     true
+  end
+
+  def render_sidebar
+    @left_column = render_to_string(:partial => 'sidebar') if @group
   end
   
   def authorized?
