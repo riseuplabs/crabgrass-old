3d6f34f2548788e968c9bc8e95462319afcb12f0
Elijah Saxon <elijah@riseup.net>
2008-12-23 11:47:43 -0800

    make haml and backgroundrb optional plugins.

--- config/initializers/background.rb
+++ config/initializers/background.rb
@@ -3,6 +3,6 @@
 # returns true if backgrounDRb daemon is running
 #
 def backgroundrb_running?
-  MiddleMan.all_worker_info.to_a.first[1].any?
+  defined?(MiddleMan) and MiddleMan.all_worker_info.to_a.first[1].any?
 end
 
