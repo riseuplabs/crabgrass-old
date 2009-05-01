2614221846b82e3bad4f792ceee3ad7400d6849b
Elijah Saxon <elijah@riseup.net>
2009-05-01 01:56:53 -0700

    a gaggle of tiny changes.

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -15,7 +15,7 @@ class AccountController < ApplicationController
   end
 
   def login
-    if !( params[:redirect].empty? || params[:redirect] =~ /^http:\/\/#{request.domain}/ || params[:redirect] =~ /^\//)
+    if !( params[:redirect].empty? || params[:redirect] =~ /^https?:\/\/#{request.domain}/ || params[:redirect] =~ /^\//)
       flash_message(:title => 'Illegal redirect'[:illegal_redirect],
       :error => "You are trying to redirect to a foreign domain (:url) after your login. For security reasons we have removed this parameter from the URL."[:redirect_to_foreign_domain]%{ :url => params.delete(:redirect)})
       redirect_to params and return
