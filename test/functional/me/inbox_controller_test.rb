58edcee1dab3a81ede4855b5291eaaee0d66574b
jrw <jrw@lieblingscomputer2.brueckenschlaeger>
2009-05-03 01:22:46 +0200

    bugfix for useless bug that messed up inbox search. included basic tests for inbox_controller to check that searches at least throw no errors

--- test/functional/me/inbox_controller_test.rb
+++ test/functional/me/inbox_controller_test.rb
@@ -50,6 +50,39 @@ class InboxControllerTest < Test::Unit::TestCase
                "page #{removed_page_id} shouldn't be in the inbox anymore"
   end
 
+  
+  def test_search
+    login_as :blue
+    @user = users(:blue)
+    assert @user, 'the user should exist'
+    assert @user.groups, 'the user should have at least one group for this test'
+    assert @user.contacts, 'the user should have at least one contact for this test'
+    # test some various calls on the search methods, that could come from the search_controller by post
+    
+    #TODO: This test should also test if the right pages come back as result. This test only checks if posting a search leads into an error so far.
+    
+    
+    # test with empty parameters (=> showing all pages}
+    post :search, :search => { :text => "", :type => "", :page_state => "", :person => "", :group => "" }, :commit => "Search"   
+    # perform a text search
+    post :search, :search => { :text => "baum", :type => "", :page_state => "", :person => "", :group => "" }, :commit => "Search"
+    
+    # perform a search for user and group
+    post :search, :search => { :text => "", :type => "", :page_state => "", :person => @user.contacts.first.login, :group => @user.groups.first.name }, :commit => "Search"
+    
+    # testing all pages with all page states    
+    #todo: this should be dynamically loaded from the sites available pages
+    page_types = ["asset","video","discussion","gallery","message","vote","survey","task","wiki"]
+    #todo: this also should maybe loaded dynamically
+    page_states = ["unread","pending","starred"]
+    page_types.each do |type|
+      page_states.each do |state|
+            post :search, :search => { :text => "", :type => type, :page_state => state, :person => "", :group => ""}, :commit => "Search"
+      end
+    end
+  end
+  
+  
   # stubs for testing actions
   # fill in when we know what they are supposed to do
   def test_update
