b98c5bf086b492855b63df74c7c7d39462a2bc1a
Elijah Saxon <elijah@riseup.net>
2009-05-20 02:37:17 -0700

    added title tooltop for public checkbox in the superadmin mod.

--- mods/super_admin/app/helpers/base_page_helper.rb
+++ mods/super_admin/app/helpers/base_page_helper.rb
@@ -7,14 +7,13 @@ module BasePageHelper
       checked = @page.public? || @page.public_requested?
       url = {:controller => 'base_page/participation',:action => 'update_public', :page_id => @page.id, :public => (!checked).to_s}
       if !@page.public_requested?
-        checkbox_line = sidebar_checkbox('make public'.t, checked, url, li_id, checkbox_id)
+        checkbox_line = sidebar_checkbox('make public'.t, checked, url, li_id, checkbox_id, :title => "If checked, anyone may view this page."[:public_checkbox_help])
       elsif !@page.public?
-        checkbox_line = sidebar_checkbox('make public (pending)'.t, checked, url, li_id, checkbox_id)
+        checkbox_line = sidebar_checkbox('make public (pending)'.t, checked, url, li_id, checkbox_id, :title => "If checked, anyone may view this page."[:public_checkbox_help])
       end
       content_tag :li, checkbox_line, :id => li_id, :class => 'small_icon'
     end
   end
-
   
   def public_requested_line
     return 'public (pending)'
