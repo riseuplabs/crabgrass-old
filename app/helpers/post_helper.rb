37b537125d3110a10df7737c8ee0715fd5097140
Azul <azul@riseup.net>
2009-05-26 22:48:42 +0200

    edit and star actions in helper instead of post template
    they fit better in a helper anyway and they can be overwritten with
    cc.net specifica this way.

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -4,4 +4,26 @@ module PostHelper
     content_tag(:tr, content_tag(:td, pagination_links(@posts, :param_name => 'posts'), :colspan => 2)) if @posts.any?
   end
 
+  # This function can be overwritten by mods to add information to the 
+  # author display next to a post.
+  def author_info(post)
+  end
+
+  def edit_post_action(post)
+    return unless logged_in? and post.user_id == current_user.id
+    content_tag :div, :style=>'display: block', :class=>'post_action_icon' do
+      link_to_remote_icon('pencil', {:url => {:controller => 'posts', :action => 'edit', :id => post.id}})
+    end
+  end
+
+  def star_post_action(post)
+    return unless logged_in? and post.user_id != current_user.id and current_user.may?(:comment,@page)
+    content_tag :div, :class=>'post_action_icon' do
+      if !post.starred_by?(current_user)
+        link_to_remote_icon('star_plus', :url=>{:controller=>'posts', :action=>'twinkle', :id=>post.id})
+      else
+        link_to_remote_icon('star_minus', :url=>{:controller=>'posts', :action=>'untwinkle', :id=>post.id})
+      end
+    end
+  end
 end
