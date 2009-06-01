cf6175c855caa822e7a70594dac537d10574cc9a
Elijah Saxon <elijah@riseup.net>
2009-06-01 03:43:07 -0700

    change formatting reference to read 'Editing Help'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -147,7 +147,7 @@ module ApplicationHelper
   end
 
   def formatting_reference_link
-   %Q{<div class='formatting_reference'><a href="/static/greencloth" onclick="quickRedReference(); return false;">%s</a></div>} % "formatting reference"[:formatting_reference_link]
+   %Q{<div class='formatting_reference'><a class="small_icon help_16" href="/static/greencloth" onclick="quickRedReference(); return false;">%s</a></div>} % "formatting reference"[:formatting_reference_link]
   end
 
 end
