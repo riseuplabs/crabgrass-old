2e8aadb954539bc113dbc4b233a9336debff456b
Elijah Saxon <elijah@riseup.net>
2009-06-01 03:53:04 -0700

    replace 'join group' with 'request to join group', in some cases.

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -29,11 +29,11 @@ module GroupHelper
       if current_user.direct_member_of? @group
         return nil
       elsif current_user.member_of? @group
-        # if you are an indirect member of this group then (1) it is a committee and (2) you are a member of the group containing it, so you may add yourself to the committee from the edit page.  This may not be true when networks are implemented.
-        link_to "join %s"[:join_group] % @group_type, 
+        # if you are an indirect member of this group then (1) it is a committee and (2) you are a member of the group containing it, so you may add yourself to the committee from the edit page. 
+        link_to "join {group_type}"[:join_group_link, @group_type], 
           url_for(:controller => '/membership', :action => 'join', :id => @group)
       elsif @group.profiles.visible_by(current_user).may_request_membership?
-        link_to "join %s"[:join_group] % @group_type, 
+        link_to "request to join {group_type}"[:request_join_group_link, @group_type], 
          url_for(:controller => '/requests', :action => 'create_join', :group_id => @group.id)
       end
     end
