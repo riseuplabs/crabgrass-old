4094218478bb10fcc23b732a108e2ea0d8c9e839
Juozas Gaigalas <juozas@hobo23.(none)>
2009-06-05 15:51:54 -0700

    flex_image updated some files automatically

--- vendor/plugins/flex_image/test/rails_root/app/models/avatar.rb
+++ vendor/plugins/flex_image/test/rails_root/app/models/avatar.rb
@@ -1,3 +1,10 @@
+=begin
+create_table "avatars", :force => true do |t|
+  t.binary        "image_file_data"
+  t.boolean       "public,            :default => false
+end
+=end
+
 class Avatar < ActiveRecord::Base
   acts_as_fleximage :image_directory => 'public/uploads'
   validates_presence_of :username
