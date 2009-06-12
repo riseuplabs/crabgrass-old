ded7f8c28e0f31d16351ab09d328f1e334d553a3
Elijah Saxon <elijah@riseup.net>
2009-06-12 15:31:25 -0700

    group helper: remove old group_helper and fix permission type.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -28,7 +28,7 @@ module GroupsHelper
 
   def join_group_link
     return unless logged_in? and !current_user.direct_member_of? @group
-    if may_join_membership?
+    if may_join_memberships?
       link_to("join {group_type}"[:join_group, @group.group_type], {:controller => :membership, :action => 'join', :group_id => @group.id})
     elsif may_create_join_request?
       link_to("request to join {group_type}"[:request_join_group_link, @group.group_type], {:controller => :requests, :action => 'create_join', :group_id => @group.id})
