3675416864ec8e7a88958092e52100fae6851a61
Azul <azul@riseup.net>
2009-06-15 22:53:17 +0200

    deploy.rb now points to the rewrite branch.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :application, "crabgrass"
 set :user, "crabgrass"
 
 set :repository, "gitosis@labs.riseup.net:unicef.git"
-set :branch, "cc"
+set :branch, "cc-rewrite"
 
 deploy_host = "yellowhammer.riseup.net"
 staging_host = "209.234.253.11"
