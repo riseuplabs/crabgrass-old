26376966e1f8bac3f13ed41541501805b0541f69
niklas <niklas@brueckenschlaeger.de>
2009-06-16 01:40:12 +0200

    Custom Appearances: Integration in admin panel ; works also for sites without existant appearance now. I had to change parts of the custom_appearances permissions, please take someone a look at it for QA.

--- app/controllers/custom_appearances_controller.rb
+++ app/controllers/custom_appearances_controller.rb
@@ -6,10 +6,19 @@ class CustomAppearancesController < ApplicationController
 
   before_filter :view_setup, :except => [:favicon, :available]
   before_filter :login_required, :except => [:favicon]
-  prepend_before_filter :fetch_data, :except => [:favicon]
+  prepend_before_filter :fetch_data, :except => [:favicon, :new]
+  
+  def new
+    unless current_site.custom_appearance
+      current_site.create_custom_appearance
+      current_site.save
+    end
+    redirect_to edit_custom_appearance_url(current_site.custom_appearance)
+  end
 
   # GET edit_custom_appearance_url
   def edit
+    @active = 'siteadmin_custom_appearances'
     render :layout => 'admin'
   end
 
