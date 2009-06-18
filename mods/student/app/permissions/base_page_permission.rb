0943dc910c1198048e68b3714eb818b62b689c1c
Azul <azul@riseup.net>
2009-06-18 19:36:35 +0200

    CUSTOM:fix show_page permission, minor conf change

--- mods/student/app/permissions/base_page_permission.rb
+++ mods/student/app/permissions/base_page_permission.rb
@@ -2,7 +2,9 @@ module BasePagePermission
   # teachers should be able to see all of their students pages.
 
   def may_show_page?(page = @page)
-    !page or current_user.may?(:view, page) or
+    !page or
+    page.public? or
+    current_user.may?(:view, page) or
     may_view_as_teacher?(page)
   end
 
@@ -10,6 +12,6 @@ module BasePagePermission
 
   def may_view_as_teacher?(page)
     user_ids = page.user_participations.collect{|p|p.user_id}
-    (user_ids & current_user.student_id_cache).empty
+    !(user_ids & current_user.student_id_cache).empty?
   end
 end
