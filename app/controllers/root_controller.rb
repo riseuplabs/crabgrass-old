3d9028fa9fdfb2344dd94087afae5e827119f89a
Elijah Saxon <elijah@riseup.net>
2009-06-24 02:52:53 -0700

    fancy tabs for site home

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -7,6 +7,8 @@ class RootController < ApplicationController
   stylesheet 'wiki_edit'
   javascript 'wiki_edit'
   permissions 'groups/base'
+  before_filter :login_required, :except => ['index']
+  before_filter :fetch_network
 
   def index
     if !logged_in?
@@ -18,17 +20,48 @@ class RootController < ApplicationController
     end
   end
 
+  ##
+  ## TAB CONTENT, PULLED BY AJAX
+  ##
+
+  def featured
+    update_page_list('featured_panel',
+      :pages => paginate('featured_by', @group.id, 'descending', 'updated_at')
+    )
+  end
+
+  def most_viewed
+    update_page_list('most_viewed_panel',
+      :pages => paginate('descending', 'views'),
+      :columns => [:views, :icon, :title, :last_updated], 
+      :sortable => false
+    )
+  end
+
+  def announcements
+    update_page_list('announcements_panel', 
+      :pages => paginate('descending','created_at', :flow => :announcement)
+    )
+  end
+
+  def recent_pages
+    update_page_list('recent_pages_panel', 
+      :pages => paginate('descending', 'updated_at'),
+      :columns => [:stars, :icon, :title, :last_updated], 
+      :sortable => false,
+      :show_time_dividers => true
+    )
+  end
+
   protected
 
+  def authorized?
+    true
+  end
+
   def site_home
-    @group = current_site.network
     @active_tab = :home
-
-    @recent_pages = Page.find_by_path(['descending', 'updated_at', 'limit','20'], options_for_group(@group))
-    @most_viewed_pages = Page.find_by_path(['descending', 'views_count', 'limit','10'], options_for_group(@group))
-    @announcements = Page.find_by_path('limit/3/descending/created_at', options_for_group(@group, :flow => :announcement))
     @group.profiles.public.create_wiki unless @group.profiles.public.wiki
-
     render :template => 'root/site_home'    
   end
 
@@ -37,6 +70,25 @@ class RootController < ApplicationController
     @active_tab = :home
     render :template => 'account/index'
   end
+
+  def fetch_network
+    @group = current_site.network if current_site and current_site.network
+  end
+
+  def paginate(*args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    Page.paginate_by_path(args, options_for_group(@group, {:page => params[:page], :per_page => 5}.merge(options)))
+  end
+
+  def update_page_list(target, locals)
+    render :update do |page|
+      page.replace_html target, :partial => 'pages/list', :locals => locals
+    end
+  end
+
+  ##
+  ## lists of active groups and users. used by the view. 
+  ##
 
   helper_method :most_active_groups
   def most_active_groups
@@ -57,10 +109,6 @@ class RootController < ApplicationController
   def recently_active_users
     User.most_active_on(current_site, Time.now - 30.days).not_inactive.find(:all, :limit => 10)
   end
-
-#    @groups = Group.visible_by(current_user).only_groups.recent.find(:all, :limit => 10)
-#    @users = User.most_active_on(current_site, Time.now - 30.days).not_inactive.find(:all, :limit => 10)
-
 
 end
 
