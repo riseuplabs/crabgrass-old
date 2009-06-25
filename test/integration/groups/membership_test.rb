dbe7e4845f5a0822db9c7d070d04b148db83c3ba
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-06-24 23:07:44 -0700

    Fixed wrong test class name

--- test/integration/groups/membership_test.rb
+++ test/integration/groups/membership_test.rb
@@ -1,6 +1,6 @@
 require "#{File.dirname(__FILE__)}/../../test_helper"
 
-class Me::GroupsTest < ActionController::IntegrationTest
+class Groups::MembershipTest < ActionController::IntegrationTest
   def test_joining_network_when_indirect_member
     login 'dolphin'
 
