3102e4802d8eeb9f2601645d37e1a3d54e3a63e5
Azul <azul@riseup.net>
2009-06-26 19:13:57 +0200

    updated schema this should fix CruiseControl

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090626071633) do
+ActiveRecord::Schema.define(:version => 20090626081151) do
 
   create_table "activities", :force => true do |t|
     t.integer  "subject_id",   :limit => 11
@@ -132,6 +132,16 @@ ActiveRecord::Schema.define(:version => 20090626071633) do
     t.integer  "favicon_id",        :limit => 11
   end
 
+  create_table "dailies", :force => true do |t|
+    t.integer "page_id",    :limit => 11
+    t.integer "views",      :limit => 11
+    t.integer "ratings",    :limit => 11
+    t.integer "edits",      :limit => 11
+    t.date    "created_at"
+  end
+
+  add_index "dailies", ["page_id"], :name => "index_dailies_on_page_id"
+
   create_table "discussions", :force => true do |t|
     t.integer  "posts_count",      :limit => 11, :default => 0
     t.datetime "replied_at"
@@ -228,6 +238,16 @@ ActiveRecord::Schema.define(:version => 20090626071633) do
 
   add_index "groups", ["name"], :name => "index_groups_on_name"
   add_index "groups", ["parent_id"], :name => "index_groups_parent_id"
+
+  create_table "hourlies", :force => true do |t|
+    t.integer  "page_id",    :limit => 11
+    t.integer  "views",      :limit => 11
+    t.integer  "ratings",    :limit => 11
+    t.integer  "edits",      :limit => 11
+    t.datetime "created_at"
+  end
+
+  add_index "hourlies", ["page_id"], :name => "index_hourlies_on_page_id"
 
   create_table "im_addresses", :force => true do |t|
     t.integer "profile_id", :limit => 11
@@ -684,6 +704,9 @@ ActiveRecord::Schema.define(:version => 20090626071633) do
     t.integer  "user_id",    :limit => 11
     t.integer  "group_id",   :limit => 11
     t.datetime "tracked_at"
+    t.boolean  "views"
+    t.boolean  "edits"
+    t.boolean  "stars"
   end
 
   execute "ALTER TABLE trackings ENGINE = MyISAM"
