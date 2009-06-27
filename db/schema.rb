d87e88543cb5d117f559679d1aa4a31b01f22a6d
Elijah Saxon <elijah@riseup.net>
2009-06-27 03:30:37 -0700

    bumped the schema version

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090626232457) do
+ActiveRecord::Schema.define(:version => 20090626234003) do
 
   create_table "activities", :force => true do |t|
     t.integer  "subject_id",   :limit => 11
