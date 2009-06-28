3a1b19dbdec79e8e90cf6578fc12466caf33c26c
Elijah Saxon <elijah@riseup.net>
2009-06-28 02:23:17 -0700

    crabgrass code changes for upgrade of thinking sphinx

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -169,23 +169,15 @@ class Test::Unit::TestCase
   rake RAILS_ENV=test db:test:prepare db:fixtures:load  # (should not be necessary, but always a good first step)
   rake RAILS_ENV=test ts:index ts:start                 # (needed to build the sphinx index and start searchd)
   rake test:functionals
-See also doc/SPHINX_README"
+See also doc/SPHINX"
       @@sphinx_hints_printed = true
     end
 
   end
 
   def sphinx_working?(test_name="")
-    if `which searchd`.empty?
-      print 'skip' #(skipping %s: sphinx not installed)' % test_name
-      print_sphinx_hints
-      false
-    elsif !sphinx_running?
-      print 'skip' #'(skipping %s: sphinx not running)' % test_name
-      print_sphinx_hints
-      false
-    elsif !ThinkingSphinx.updates_enabled?
-      print 'skip' #'(skipping %s: sphinx updated disabled)' % test_name
+    if !ThinkingSphinx.sphinx_running?
+      print 'skip'
       print_sphinx_hints
       false
     else
