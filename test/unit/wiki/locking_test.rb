71b9d5ec7c459b46aa789b210acb90ff1e7ec337
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-07-02 16:35:36 -0700

    non passing tests for new wiki locking interface

---
+++ test/unit/wiki/locking_test.rb
@@ -0,0 +1,183 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class Wiki::LockingTest < Test::Unit::TestCase
+  fixtures :users, :wikis
+
+  context "A Wiki with many sections" do
+    setup do
+      @wiki = wikis(:multi_section)
+      @blue = users(:blue)
+      @red = users(:red)
+    end
+
+    should "raise WikiLockException when locking a non-existant section" do
+      assert_raises WikiLockException {@wiki.lock 'bad-nontexistant-section-header', @blue}
+    end
+
+    context "when a user locks a section-two" do
+      setup { @wiki.lock 'section-two', @blue }
+      end
+
+      context "and that user unlocks section-two" do
+        setup { @wiki.unlock 'section-two', @blue }
+
+        should "appear the same to that user and a different user" do
+          assert_same_elements @wiki.sections_open_for(@blue), @wiki.sections_open_for(@red)
+          assert_same_elements @wiki.sections_locked_for(@blue), @wiki.sections_locked_for(@red)
+        end
+
+        should "appear to a different user that all sections can be edited and none are locked" do
+          assert_same_elements @wiki.sections_open_for(@red), @wiki.all_sections
+          assert @wiki.sections_locked_for(@red).empty?
+        end
+      end
+
+      context "for that user" do
+        setup {@user = @blue}
+        test_open_sections = [:document, 'top-oversection',
+          'section-two', 'subsection-for-section-two', 'section-one', 'second-oversection']
+
+        test_open_sections.each { |section_heading|
+          should "have the #{section_heading.inspect} section open" do
+            assert @wiki.sections_open_for(@user).include?(section_heading)
+            assert !@wiki.sections_locked_for(@user).include?(section_heading)
+          end
+
+          should "raise no errors when locking #{section_heading.inspect} section" do
+            assert_nothing_raised {@wiki.lock section_heading, @user}
+          end
+
+          should "raise no errors when unlocking #{section_heading.inspect} section" do
+            assert_nothing_raised {@wiki.unlock section_heading, @user}
+          end
+        }
+
+      end
+
+      context "for a different user" do
+        setup {@user = @red}
+
+        test_closed_sections = [:document, 'top-oversection', 'section-two', 'subsection-for-section-two']
+
+        test_closed_sections.each { |section_heading|
+          should "have the #{section_heading.inspect} section closed" do
+            assert !@wiki.sections_open_for(@user).include?(section_heading)
+            assert @wiki.sections_locked_for(@user).include?(section_heading)
+          end
+        
+          should "raise WikiLockException when locking #{section_heading.inspect} section" do
+            assert_raises WikiLockException {@wiki.lock section_heading, @user}
+          end
+        
+          should "raise WikiLockException when unlocking #{section_heading.inspect} section" do
+            assert_raises WikiLockException {@wiki.unlock section_heading, @user}
+          end
+        }
+
+        should "have the neighborhing sections open" do
+          assert @wiki.sections_open_for(@user).include?('section-one')
+          assert !@wiki.sections_locked_for(@user).include?('section-one')
+
+          assert @wiki.sections_open_for(@user).include?('second-oversection')
+          assert !@wiki.sections_locked_for(@user).include?('second-oversection')
+        end
+
+        should "raise no errors when locking and unlocking neighboring section" do
+          assert_nothing_raised {@wiki.lock 'section-one', @user }
+          assert_nothing_raised {@wiki.lock 'second-oversection', @user }
+
+          assert_nothing_raised {@wiki.unlock 'section-one', @user }
+          assert_nothing_raised {@wiki.unlock 'second-oversection', @user }
+        end
+
+        should "not raise WikiLockException when trying to break the lock for 'section-two'" do
+          assert_nothing_raised {@wiki.unlock section_heading, @user, :break => true}
+        end
+      end
+    end
+
+    context "when a user locks the whole document" do
+      setup {@wiki.lock :document, @blue}
+
+      context "and that user unlocks the whole document" do
+        setup {@wiki.lock :document, @blue}
+
+        should "appear the same to that user and a different user" do
+          assert_same_elements @wiki.sections_open_for(@blue), @wiki.sections_open_for(@red)
+          assert_same_elements @wiki.sections_locked_for(@blue), @wiki.sections_locked_for(@red)
+        end
+
+        should "appear to a different user that all sections can be edited and none are locked" do
+          assert_same_elements @wiki.sections_open_for(@red), @wiki.all_sections
+          assert @wiki.sections_locked_for(@red).empty?
+        end
+      end
+
+      should "appear to that user that all sections can be edited and none are locked" do
+        assert_same_elements @wiki.sections_open_for(@blue), @wiki.all_sections
+        assert @wiki.sections_locked_for(@blue).empty?
+      end
+
+      should "appear to a different user that no sections can be edited and all are locked" do
+        assert @wiki.sections_open_for(@red).empty?
+        assert_same_elements @wiki.sections_locked_for(@red), @wiki.all_sections
+      end
+
+      should "raise an exception (and keep the same state) when a different user tries to lock the document" do
+        assert_raises WikiLockException {@wiki.lock :document, @red}
+
+        assert_same_elements @wiki.sections_open_for(@blue), @wiki.all_sections
+        assert @wiki.sections_open_for(@red).empty?
+        assert_same_elements @wiki.sections_open_for(@red), @wiki.all_sections
+      end
+
+      should "raise an exception (and keep the same state) when a different user tries to lock a section" do
+        assert_raises WikiLockException {@wiki.lock 'section_one', @red}
+
+        assert_same_elements @wiki.sections_open_for(@blue), @wiki.all_sections
+        assert @wiki.sections_open_for(@red).empty?
+        assert_same_elements @wiki.sections_open_for(@red), @wiki.all_sections
+      end
+
+      context "and that user locks a section" do
+        setup {@wiki.lock 'section_one', @blue}
+
+        should "appear to that user that all sections can be edited and none are locked" do
+          assert_same_elements @wiki.sections_open_for(@blue), @wiki.all_sections
+          assert @wiki.sections_locked_for(@blue).empty?
+        end
+
+        should "appear to a different user that no sections can be edited and all are locked" do
+          assert @wiki.sections_open_for(@red).empty?
+          assert_same_elements @wiki.sections_locked_for(@red), @wiki.all_sections
+        end
+
+        context "and then unlocks that section" do
+          setup {@wiki.lock 'section-one', @blue}
+
+          should "appear to that user that all sections can be edited and none are locked" do
+            assert_same_elements @wiki.sections_open_for(@blue), @wiki.all_sections
+            assert @wiki.sections_locked_for(@blue).empty?
+          end
+
+          should "appear to a different user that no sections can be edited and all are locked" do
+            assert @wiki.sections_open_for(@red).empty?
+            assert_same_elements @wiki.sections_locked_for(@red), @wiki.all_sections
+          end
+        end
+      end
+    end
+    #
+    # def test_lock_race_condition
+    #   w = wikis(:multi_section)
+    #
+    #   w.lock(Time.now, users(:orange), 'section-one')
+    #   assert_raises WikiLockException do
+    #     w2 = Wiki.find(w.id)
+    #     w2.lock(Time.now, users(:blue), 'section-one')
+    #   end
+    #   assert_equal ['section-one'], w.reload.locked_sections
+    #   assert_equal users(:orange).id, w.locked_by_id('section-one')
+    # end
+
+end
--- test/unit/wiki_test.rb
+++ test/unit/wiki_test.rb
@@ -1,54 +1,11 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class WikiTest < Test::Unit::TestCase
-
   fixtures :users, :wikis
 
-  def setup
-    @orange_id = users(:orange).id
-    @blue_id = users(:blue).id
+  should "have good associations" do
+    assert check_associations(Wiki)
   end
-
-  def test_creation_group_space
-    g = Group.create! :name => 'robots'
-
-    a = WikiPage.create :title => 'x61'
-    a.add g; a.save
-
-    b = WikiPage.create :title => 'x61'
-    b.add g;
-
-    assert_equal 'x61', a.name, 'name should equal title'
-    assert b.name_taken?, 'name should already be taken'
-    assert !b.valid?, 'should not be able to have two wikis with the same name'
-  end
-
-  def test_creation_user_space
-    a = WikiPage.create :title => 'x61', :owner => 'blue', :user => users(:blue)
-    b = WikiPage.create :title => 'x61', :owner => 'blue', :user => users(:blue)
-
-    assert_equal 'x61', a.name, 'name should equal title'
-    assert b.name_taken?, 'name should already be taken'
-    assert !b.valid?, 'should not be able to have two wikis with the same name'
-  end
-
-  def test_lock
-    w = Wiki.create :body => 'watermelon'
-    w.lock(Time.now, users(:blue))
-
-    assert w.locked?, 'locked should be true'
-    assert w.editable_by?(users(:blue)), 'blue should be able to edit wiki'
-    assert !w.editable_by?(users(:red)), 'red should not be able to edit wiki'
-
-    assert !w.editable_by?(users(:blue), 'section-one'), 'blue should not be able to edit wiki section one'
-    assert !w.editable_by?(users(:red), 'section-one'), 'red should not be able to edit wiki section one'
-
-    w.unlock
-
-    assert w.editable_by?(users(:blue)), 'blue should be able to edit wiki'
-    assert w.editable_by?(users(:red)), 'red should be able to edit wiki'
-  end
-
 
   def test_saving
     w = Wiki.create :body => 'watermelon'
@@ -67,135 +24,56 @@ class WikiTest < Test::Unit::TestCase
     assert_nothing_raised do
       w.smart_save! :body => 'catelope', :user => users(:blue)
     end
-
   end
 
-  def test_section_lock
-    w = wikis(:multi_section)
+  context "A wiki" do
+    setup {@wiki = Wiki.new }
 
-    w.lock(Time.now, users(:orange), 'section-one')
-    assert w.locked?('section-one'), 'locked section-one should be true'
+    context "A wiki with four versions" do
+      setup do
+        @wiki = Wiki.create! :body => '1111'
+        @wiki.smart_save!(:body => '2222', :user => users(:red))
+        @wiki.smart_save!(:body => '3333', :user => users(:green))
+        @wiki.smart_save!(:body => '4444', :user => users(:blue))
+      end
+      should_change "versions count", :from => 0, :to => 4 do @wiki.versions.size end
 
-    assert w.editable_by?(users(:orange), 'section-one'), 'orange should be able to edit wiki section one'
-    assert !w.editable_by?(users(:red), 'section-one'), 'red should not be able to edit wiki section one'
+      should "find version 1 body" do
+        assert_equal '1111', @wiki.versions.find_by_version(1).body
+      end
 
-    assert w.editable_by?(users(:orange), 'section-two'), 'orange should be able to edit wiki section two'
-    assert w.editable_by?(users(:blue), 'section-two'), 'blue should be able to edit wiki section two'
+      should "find version 4 body" do
+        assert_equal '4444', @wiki.versions.find_by_version(4).body
+      end
 
-    assert w.editable_by?(users(:orange), :all), 'orange should be able to edit the whole wiki'
-    assert !w.editable_by?(users(:red), :all), 'red should not be able to edit the whole wiki'
+      context "after a soft revert to an older version" do
+        setup {@wiki.revert_to_version(3, users(:purple)) }
 
-    w.unlock('section-one')
+        should "create a new version equal to the older version" do
+          assert_equal '3333', @wiki.versions.find_by_version(5).body
+        end
 
-    assert w.editable_by?(users(:orange), 'section-one'), 'orange should be able to edit wiki section one'
-    assert w.editable_by?(users(:red), 'section-one'), 'red should be able to edit wiki section one'
-  end
+        should "revert wiki body" do
+          assert_equal '3333', @wiki.body
+        end
+      end
 
-  def test_locked_by_id
-    w = wikis(:multi_section)
+      context "after a hard revert to an older version" do
+        setup {@wiki.revert_to_version!(2, users(:purple))}
 
-    assert_nil w.locked_by_id(:all), "no one should be the locker of the whole wiki body"
-    assert_nil w.locked_by_id('section-two'), "no one should be the locker of the wiki section two"
-    assert_nil w.locked_by_id('section-one'), "no one should be the locker of the wiki section one"
+        should "revert wiki body" do
+          assert_equal '2222', @wiki.body
+        end
 
-    # lock all sections
-    w.lock(Time.now, users(:orange), :all)
+        should "delete all newer versions" do
+          assert_equal 2, @wiki.versions(true).size
+        end
 
-    assert_equal @orange_id, w.locked_by_id(:all), "orange should be the locker of the whole wiki body"
-    assert_equal @orange_id, w.locked_by_id('section-one'), "orange should appear as the locker of wiki section one"
-    assert_equal @orange_id, w.locked_by_id('section-two'), "orange should appear as the locker of wiki section two"
-  end
-
-  def test_section_locked_by_id
-    w = wikis(:multi_section)
-
-    assert_nil w.locked_by_id(:all), "no one should be the locker of the whole wiki body"
-    assert_nil w.locked_by_id('section-two'), "no one should be the locker of the wiki section two"
-    assert_nil w.locked_by_id('section-one'), "no one should be the locker of the wiki section one"
-
-    # lock one section
-    w.lock(Time.now, users(:orange), 'section-two')
-
-    assert_equal @orange_id, w.locked_by_id(:all), "orange should be the locker of the whole wiki body"
-    assert_nil w.locked_by_id('section-one'), "no one should appear as the locker of wiki section one"
-    assert_equal @orange_id, w.locked_by_id('section-two'), "orange should appear as the locker of wiki section two"
-  end
-
-  def test_list_of_locked_sections
-    w = wikis(:multi_section)
-
-    # lock sections by different users
-    w.lock(Time.now, users(:orange), 'section-one')
-    w.lock(Time.now, users(:blue), 'section-two')
-
-    # check list of all sections and per-users lists
-    assert_equal ['section-two', 'section-one'].sort, w.locked_sections.sort, "wiki should have sections one and two locked"
-    assert_equal 'section-one', w.currently_editing_section(users(:orange)), "wiki should list section one as locked by orange"
-    assert_equal ['section-two'], w.locked_sections_not_by(users(:orange)), "wiki should list section two as locked by users other than orange"
-  end
-
-  def test_lock_race_condition
-    w = wikis(:multi_section)
-
-    w.lock(Time.now, users(:orange), 'section-one')
-    assert_raises WikiLockException do 
-      w2 = Wiki.find(w.id)
-      w2.lock(Time.now, users(:blue), 'section-one')
+        should "keep the version it was reverted to" do
+          assert_equal '2222', @wiki.versions.find_by_version(2).body
+        end
+      end
     end
-    assert_equal ['section-one'], w.reload.locked_sections
-    assert_equal users(:orange).id, w.locked_by_id('section-one')
-  end
-
-  def test_can_lock_only_one_section
-      w = wikis(:multi_section)
-
-      # lock one section
-      w.lock(Time.now, users(:orange), 'section-one')
-      # should be locked
-      assert_equal @orange_id, w.locked_by_id('section-one'), "orange should appear as the locker of wiki section one"
-
-      # locked different section
-      w.lock(Time.now, users(:orange), 'section-two')
-      # should be unlocked now
-      assert_nil w.locked_by_id('section-one'), "orange should not appear as the locker of wiki section one"
-      # section two should be locked
-      assert_equal @orange_id, w.locked_by_id('section-two'), "orange should appear as the locker of wiki section two"
-      assert w.locked_by?(users(:orange), 'section-two'), "orange should appear as the locker of wiki section two"
-  end
-
-  def test_wiki_page
-  end
-
-  def test_reverting
-    wiki = Wiki.create! :body => '1111'    
-    wiki.smart_save!(:body => '2222', :user => users(:red))
-    wiki.smart_save!(:body => '3333', :user => users(:green))
-    wiki.smart_save!(:body => '4444', :user => users(:blue))
-    assert_equal 4, wiki.versions.size
-    assert_equal '1111', wiki.versions.find_by_version(1).body
-    assert_equal '4444', wiki.versions.find_by_version(4).body
-
-    # soft revert
-    wiki.revert_to_version(3, users(:purple))
-    assert_equal '3333', wiki.versions.find_by_version(5).body
-
-    # hard revert
-    wiki.revert_to_version!(4, users(:purple))
-    assert_equal '4444', wiki.versions.find_by_version(4).body
-    assert_equal 4, wiki.versions(true).size
-  end
-
-  def test_nil_body
-    wiki = Wiki.new
-    assert_nothing_raised do
-      wiki.lock( Time.now, users(:blue) )
-    end
-    assert wiki.locked?
-    assert wiki.editable_by?(users(:blue))
-  end
-
-  def test_associations
-    assert check_associations(Wiki)
   end
 
 end
