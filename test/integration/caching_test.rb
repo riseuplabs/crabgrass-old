1ecaaf86e3546dec5ffba709deb6979c39e8d558
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-07-11 22:31:52 -0700

    group fragment cache keys now include user status (can the current user admin the group?) along with group status. this keeps fragments fresh for everyone. closes #973. wrote an integration test to replicate this issue

---
+++ test/integration/caching_test.rb
@@ -0,0 +1,52 @@
+require "#{File.dirname(__FILE__)}/../test_helper"
+
+class CachingTest < ActionController::IntegrationTest
+
+  def setup
+    Rails.cache.clear
+    ActionController::Base.perform_caching = true
+  end
+
+  def teardown
+    ActionController::Base.perform_caching = false
+    Rails.cache.clear
+  end
+
+  def test_joining_group_council_updates_group_info_box
+    login 'red'
+    visit '/rainbow'
+    # should save the fragment
+    assert_contain "Send Invites"
+    assert_contain "View Requests"
+
+    # create a council
+    login 'blue'
+    visit '/groups/councils/new/rainbow'
+
+    fill_in "Full Name", :with => "Indigo Council"
+    fill_in "Link Name", :with => "indigo"
+
+    click_button "Create"
+
+    assert_contain 'Group Created'
+
+    # red, who is not a council member
+    # should not see 'Send Invites' link anymore
+    # if red can see them, then cache wasn't expired
+
+    login 'red'
+    visit '/rainbow'
+
+    assert_contain 'Indigo Council'
+    assert_not_contain "Send Invites"
+    assert_not_contain "View Requests"
+
+    # blue should see the right links too if cache was expired
+    login 'blue'
+    visit '/rainbow'
+
+    assert_contain 'Indigo Council'
+    assert_contain "Send Invites"
+    assert_contain "View Requests"
+  end
+end
