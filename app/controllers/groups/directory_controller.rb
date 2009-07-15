d4123b2c0f25f09aa26e55d13985ef207bddbf59
Elijah Saxon <elijah@riseup.net>
2009-07-15 03:52:46 -0700

    now you can share pages with committees you are a member of but not a member of the parent group. also, these committees now correctly appear under the list of my groups in the directory. fixes #1020

--- app/controllers/groups/directory_controller.rb
+++ app/controllers/groups/directory_controller.rb
@@ -21,7 +21,7 @@ class Groups::DirectoryController < Groups::BaseController
   end
 
   def my
-    @groups = current_user.groups.only_type(@group_type).alphabetized('').paginate(:all, :page => params[:page])
+    @groups = current_user.primary_groups.alphabetized('').paginate(:all, :page => params[:page])
     @show_committees = true
     render_list
   end
