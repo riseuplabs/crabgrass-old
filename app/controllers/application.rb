8f9ed355b6e26721bf2fa5d7521baa32f249ab05
jrw <jrw@lieblingscomputer2.brueckenschlaeger>
2009-07-18 14:56:37 +0200

    merged conflict

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -45,7 +45,13 @@ class ApplicationController < ActionController::Base
     filter_chain.prepend_filter_to_chain(filters, :before, &block)
     filter_chain.prepend_filter_to_chain([:essential_initialization], :before, &block)
   end
-
+  
+  # renders an modalbox enabled template, to show a confirmation dialoque
+  # needs to be accessable in several controllers
+  def confirmation_popup
+    render :partial => 'common/confirmation_popup'
+  end
+  
   protected
 
   ##
