0debf533de148cf70fc67112c2dc29ce1fe202e6
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-07-20 11:58:03 -0700

    acts_as_site_limited now understands [SQL, params] format in its find_by_sql methods

--- vendor/plugins/crabgrass_acts_as_site_limited/lib/acts_as_site_limited.rb
+++ vendor/plugins/crabgrass_acts_as_site_limited/lib/acts_as_site_limited.rb
@@ -47,11 +47,18 @@ module ActsAsSiteLimited
 
         # allows the use of special macro /*SITE_LIMITED*/ for :finder_sql
         def self.prepare_site_limited_sql(sql)
+          sub_site = "1"
           if Site.current and Site.current.limited?
-            sql.sub('/*SITE_LIMITED*/', "site_id = #{Site.current.id}")
-          else
-            sql.sub('/*SITE_LIMITED*/', "1")
+            sub_site = "#{Site.current.id}"
           end
+
+          if sql.is_a? Array
+            # [SELECT x FROM y WHERE z = ?, real_z]
+            sql.first.sub('/*SITE_LIMITED*/', sub_site)
+          else
+            sql.sub('/*SITE_LIMITED*/', sub_site)
+          end
+          sql
         end
 
         def self.prepare_site_limited_options(*args)
