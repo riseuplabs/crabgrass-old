392cabdb053cfb3e0ec59f5002e5e42cfa43e79e
jrw <jrw@lieblingscomputer2.brueckenschlaeger>
2009-07-29 17:54:41 +0200

    changed limit per page for activites list from 5 to 50 (like in messages)

--- app/controllers/me/activities_controller.rb
+++ app/controllers/me/activities_controller.rb
@@ -4,7 +4,7 @@ class Me::ActivitiesController < Me::BaseController
   # display a list of recent activity
   #
   def index
-    @activities = Activity.for_dashboard(current_user).only_visible_groups.newest.unique.find(:all).paginate(:page => params[:page], :per_page => 5)
+    @activities = Activity.for_dashboard(current_user).only_visible_groups.newest.unique.find(:all).paginate(:page => params[:page], :per_page => 50)
   end
   
 end
