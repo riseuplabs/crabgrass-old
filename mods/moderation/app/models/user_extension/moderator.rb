1604bb35c80a38ab481bdbb4329acaedbed11734
Azul <azul@riseup.net>
2009-07-29 15:29:48 +0200

    finally a few first tests for moderation.

--- mods/moderation/app/models/user_extension/moderator.rb
+++ mods/moderation/app/models/user_extension/moderator.rb
@@ -1,6 +1,5 @@
 module UserExtension::Moderator
-  def moderator?(site=nil)
-    site ||= self.current_site
+  def moderator?(site=Site.current)
     if site
       self.group_ids.include?(site.moderation_group_id)
     else
