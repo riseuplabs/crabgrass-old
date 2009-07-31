04fcdaccb46f5d7b1c66697b2103f5ba0563ec2f
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-07-30 23:04:25 -0700

    fixed a double login bug with csrf and session_reset interacting + a test to confirm the bug. cleaned up a stray </ul> tag.

--- test/integration/account_test.rb
+++ test/integration/account_test.rb
@@ -1,6 +1,15 @@
 require "#{File.dirname(__FILE__)}/../test_helper"
 
 class AccountTest < ActionController::IntegrationTest
+  def setup
+    @should_allow_forgery_protection = ApplicationController.allow_forgery_protection
+    ApplicationController.allow_forgery_protection = true
+  end
+
+  def teardown
+    ApplicationController.allow_forgery_protection = @should_allow_forgery_protection
+  end
+
   def test_login_as_user
     visit '/'
     fill_in "Login name", :with => 'blue'
@@ -9,6 +18,20 @@ class AccountTest < ActionController::IntegrationTest
     click_button "Log in"
 
     assert_contain "My Dashboard"
+  end
+
+  # shouldn't raise csrf errors
+  def test_should_fail_reapeat_login_with_empty_credentials
+    visit '/account/login'
+
+    click_button "Log in"
+    assert_contain "Username or password is incorrect"
+
+    click_button "Log in"
+    assert_contain "Username or password is incorrect"
+
+    click_button "Log In"
+    assert_contain "Username or password is incorrect"
   end
 
   def atest_logout_and_login_as_different_user
