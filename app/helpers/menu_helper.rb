49c5f351d639b28cec30da5bf97d9113f9423fd4
Elijah <elijah@allison.(none)>
2009-08-04 14:10:54 -0700

    minor changes to menus

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -21,4 +21,64 @@ module MenuHelper
     render :partial => 'layouts/menu/'+partial, :locals => locals
   end
 
+  ##
+  ## MENUS
+  ##
+
+  def menu_me
+    top_menu(
+      "menu_me",
+      "Me"[:menu_me],
+      "/me/dashboard",
+      :active => @active_tab == :me,
+      :menu_items => menu_items('me')
+    )
+  end
+
+  def menu_people
+    top_menu(
+      "menu_people",
+      "People"[:menu_people],
+      people_directory_url(:friends),
+      :active => @active_tab == :people,
+      :menu_items => menu_items('boxes', {
+        :entities => current_user.friends.most_active,
+        :heading  => "My Friends"[:my_contacts],
+        :see_all_url => people_directory_url(:friends)
+      })
+    )
+  end
+
+  def menu_groups
+    top_menu(
+      "menu_groups",
+      "Groups"[:menu_groups],
+      group_directory_url,
+      :active => @active_tab == :groups,
+      :menu_items => menu_items('boxes', {
+        :entities => current_user.primary_groups.most_active,
+        :heading => 'My Groups'[:my_groups],
+        :see_all_url => group_directory_url(:action => 'my')
+      })
+    )
+  end
+
+  def menu_networks
+    top_menu(
+      "menu_networks",
+      "Networks"[:menu_networks],
+      network_directory_url,
+      :active => @active_tab == :networks,
+      :menu_items => menu_items('boxes', {
+        :entities => current_user.primary_networks.most_active,
+        :heading => 'My Networks'[:my_networks],
+        :see_all_url => network_directory_url(:action => 'my')
+      })
+    )
+  end
+
+  def menu_chat
+    top_menu "menu_chat", "Chat"[:menu_chat], '/chat', :active => @active_tab == :chat
+  end
+
 end
