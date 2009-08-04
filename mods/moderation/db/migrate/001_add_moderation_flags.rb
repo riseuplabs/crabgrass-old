f7f90eedbcd4291961b4b06fd2b5c10d80ff3bc6
Azul <azul@riseup.net>
2009-08-04 22:31:59 +0200

    CUSTOM: working around a missing migration.

--- mods/moderation/db/migrate/001_add_moderation_flags.rb
+++ mods/moderation/db/migrate/001_add_moderation_flags.rb
@@ -1,20 +1,25 @@
 class AddModerationFlags < ActiveRecord::Migration
-  def self.up
-    add_column :pages, :public_requested, :boolean, :default => false
-    add_column :pages, :vetted, :boolean, :default => false
-    add_column :pages, :yuck_count, :integer, :default => 0
 
-    add_column :posts, :vetted, :boolean, :default => false
-    add_column :posts, :yuck_count, :integer, :default => 0
+  # this migration has been applied before but the migration in db/migrate
+  # seems to be missing. So i am trying to work around it by deactivating it.
+  # It can be activated again after we have migrated past it on live install.
+  #  --azul
+  def self.up
+#    add_column :pages, :public_requested, :boolean, :default => false
+#    add_column :pages, :vetted, :boolean, :default => false
+#    add_column :pages, :yuck_count, :integer, :default => 0
+#
+#    add_column :posts, :vetted, :boolean, :default => false
+#    add_column :posts, :yuck_count, :integer, :default => 0
   end
 
   def self.down
-    remove_column :pages, :public_requested
-    remove_column :pages, :vetted
-    remove_column :pages, :yuck_count
-
-    remove_column :posts, :vetted
-    remove_column :posts, :yuck_count
+#    remove_column :pages, :public_requested
+#    remove_column :pages, :vetted
+#    remove_column :pages, :yuck_count
+#
+#    remove_column :posts, :vetted
+#    remove_column :posts, :yuck_count
   end
 end
 
