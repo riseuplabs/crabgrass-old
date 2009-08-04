77a5b9c2f9803e92576ce7d04144b90dc62e8912
Azul <azul@riseup.net>
2009-08-04 20:32:32 +0200

    fixed tests and loading groups for MenuItemsController

--- test/functional/groups/menu_items_controller_test.rb
+++ test/functional/groups/menu_items_controller_test.rb
@@ -1,45 +1,42 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
 class Groups::MenuItemsControllerTest < ActionController::TestCase
+  fixtures :groups, :users, :memberships, :menu_items
+
+  include UrlHelper
+
+  def setup
+    enable_site_testing
+  end
+
   def test_should_get_index
-    get :index
+    login_as :blue
+    get :index, :id => 'fai'
     assert_response :success
     assert_not_nil assigns(:menu_items)
   end
 
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
-
   def test_should_create_menu_item
+    login_as :blue
     assert_difference('MenuItem.count') do
-      post :create, :menu_item => { }
+      post :create, :id => 'fai',
+      :menu_item => {:link => "http://test.link", :title => "different title"}
     end
 
-    assert_redirected_to menu_item_path(assigns(:menu_item))
-  end
-
-  def test_should_show_menu_item
-    get :show, :id => menu_items(:one).id
-    assert_response :success
-  end
-
-  def test_should_get_edit
-    get :edit, :id => menu_items(:one).id
-    assert_response :success
   end
 
   def test_should_update_menu_item
-    put :update, :id => menu_items(:one).id, :menu_item => { }
-    assert_redirected_to menu_item_path(assigns(:menu_item))
+    login_as :blue
+    put :update, :id => 'fai', :menu_item_id => 1,
+      :menu_item => {:link => "http://test.link", :title => "different title"}
+    assert_response :success
   end
 
   def test_should_destroy_menu_item
+    login_as :blue
     assert_difference('MenuItem.count', -1) do
-      delete :destroy, :id => menu_items(:one).id
+      delete :destroy, :id => 'fai', :menu_item_id => menu_items(:tags).id
     end
-
-    assert_redirected_to menu_items_path
+    assert_response :success
   end
 end
