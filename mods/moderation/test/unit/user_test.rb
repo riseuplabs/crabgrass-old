654a16d2f459d6e814879b5a48aba036be3273db
Azul <azul@riseup.net>
2009-08-05 18:57:41 +0200

    minor: forgot debug mark in test

--- mods/moderation/test/unit/user_test.rb
+++ mods/moderation/test/unit/user_test.rb
@@ -12,7 +12,6 @@ class UserTest < ActiveSupport::TestCase
     assert_nothing_raised  'the migrations for the moderation mod should have been run so User#moderator? works.' do
       users(:blue).moderator?
     end
-    debugger
     assert users(:blue).moderator?, 'Blue should be a moderator - please set up the fixtures accordingly.'
   end
 
