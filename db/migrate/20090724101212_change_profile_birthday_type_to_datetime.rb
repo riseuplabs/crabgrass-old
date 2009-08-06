53af9a0e5f147eaed471daff9272a4815d77f3b9
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-08-05 20:17:12 -0700

    moved a migration file to a most recent timestamp to workout merging master/candidate with youthportal

--- db/migrate/20090724101212_change_profile_birthday_type_to_datetime.rb
+++
@@ -1,9 +0,0 @@
-class ChangeProfileBirthdayTypeToDatetime < ActiveRecord::Migration
-  def self.up
-    change_column :profiles, :birthday, :datetime
-  end
-
-  def self.down
-    change_column :profiles, :birthday, :string
-  end
-end
