10e3b647f665d5bc05c1c95e8a9db86f471ba857
Azul <azul@riseup.net>
2009-08-07 14:50:28 +0200

    fix returning to admin panel after becoming user.
    * attempted to test but problems with integration tests for mods
    * mod does not seem to get loaded.
    * see mods/super_admin/test/integration/admin/base_controller_test.rb

--- mods/super_admin/test/functional/admin/users_controller_test.rb
+++ mods/super_admin/test/functional/admin/users_controller_test.rb
@@ -1,42 +1,11 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
-class UsersControllerTest < ActionController::TestCase
+class Admin::UsersControllerTest < ActionController::TestCase
 
   fixtures :users, :sites, :groups, :memberships
 
-  def setup
-    # @controller = Admin::UsersController.new
-    # @request = ActionController::TestRequest.new
-    # @response = ActionController::TestResponse.new
-  end
+  # none of the tests in here were written for the superadmin mod - they
+  # all seemed to be a copy of the normal UserControllerTests.
+  #  --azul
 
-  def test_index
-    login_as :blue
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
-  def test_show
-    login_as :blue
-    @user = users(:blue)
-    get :show, :user_id => @user.id
-    assert_response :success
-    assert_equal @user.login, assigns(:user).login
-  end
-
-  def test_new
-  end
-
-  def test_edit
-  end
-
-  def test_create
-  end
-
-  def test_update
-  end
-
-  def test_destroy
-  end
 end
