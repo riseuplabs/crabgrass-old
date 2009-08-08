5af79f00dd138e43d08b489fbd10a6e5ad8b8716
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-08-08 01:11:45 -0700

    registration country selection loads the currently saved country name

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -273,17 +273,17 @@ class AccountController < ApplicationController
       updated_location = ProfileLocation.new(params[:visible_profile][:locations].first)
       params[:visible_profile].delete(:locations)
     else
-      updated_location = ProfileLocation.new
+      updated_location = nil
     end
 
     @visible_profile.attributes = @visible_profile.attributes.merge(params[:visible_profile] || {})
-
     @visible_profile.locations[0] ||= ProfileLocation.new
-    location = @visible_profile.locations[0]
-    location.attributes = location.attributes.merge(updated_location.attributes)
+    if updated_location
+      location = @visible_profile.locations[0]
+      location.attributes = updated_location.attributes
+    end
 
     @user.setup_profile_required_info(@visible_profile, @hidden_profile)
   end
-
 
 end
