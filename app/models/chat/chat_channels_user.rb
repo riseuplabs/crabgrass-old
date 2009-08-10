6a3af89df462ddae134db400bbefa94fe0c34b09
pietro <pietro@riseup.net>
2009-08-10 01:50:25 -0700

    Create a user <--> chat channels relationship and delete all ChatChannelsUsers for a user when the user is deleted.

--- app/models/chat/chat_channels_user.rb
+++ app/models/chat/chat_channels_user.rb
@@ -1,6 +1,7 @@
 class ChatChannelsUser < ActiveRecord::Base
   set_table_name 'channels_users'
 
+  belongs_to :chat_channel
   belongs_to :channel, :class_name => 'ChatChannel', :foreign_key => 'channel_id'
   belongs_to :user
 
