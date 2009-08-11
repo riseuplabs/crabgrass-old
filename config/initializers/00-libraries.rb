f8279f088c49bae5eb509d32cdfca3f79cb3f002
Álvaro Gil <zevarito@gmail.com>
2009-08-11 18:52:46 -0300

    put undress and uglify_html under lib as submodules

--- config/initializers/00-libraries.rb
+++ config/initializers/00-libraries.rb
@@ -3,9 +3,10 @@
 end
 
 require "#{RAILS_ROOT}/lib/greencloth/greencloth.rb"
+require "#{RAILS_ROOT}/lib/undress/lib/undress/greencloth.rb"
+require "#{RAILS_ROOT}/lib/uglify_html/lib/uglify_html.rb"
 require "#{RAILS_ROOT}/lib/path_finder.rb"
 require "#{RAILS_ROOT}/lib/i18n_helpers.rb"
 
 # model extensions:
 require "#{RAILS_ROOT}/app/models/tag.rb"
-
