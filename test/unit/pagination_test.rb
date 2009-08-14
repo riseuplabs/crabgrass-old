efb8c872e4a136a845b79b828c7baa13ca23380a
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-08-14 16:13:10 -0700

    test most_viewed pagination

--- test/unit/pagination_test.rb
+++ test/unit/pagination_test.rb
@@ -8,13 +8,16 @@ class PaginationTest < Test::Unit::TestCase
     group = groups(:rainbow)
 
     # test parameters
-    group_page_ids = (group.pages.reject{|p| p.deleted?}).collect(&:id).uniq.sort
+    all_pages = (group.pages.reject{|p| p.deleted?}).uniq
+    all_pages_ids = all_pages.collect(&:id)
     per_page = 5
-    total_pages = (group_page_ids.size.to_f / per_page).ceil
-
+    total_pagination_pages = (all_pages.size.to_f / per_page).ceil
     # add trackings
-    group.pages.each do |page|
-      3.times do
+    all_pages.each_with_index do |page, index|
+      # first page gets the most views
+      # last page gets only 1 view
+      # lets us test that pagination sorts them properly
+      (all_pages.size - index).times do
         Tracking.insert_delayed(:current_user => user, :user => user, :group => group, :page => page, :action => :view)
       end
     end
@@ -35,8 +38,8 @@ class PaginationTest < Test::Unit::TestCase
 
     pages = Page.paginate_by_path(["most_views", "30", "days"], paginate_options)
 
-    assert_equal group_page_ids.size, pages.size
-    assert_equal group_page_ids[0, per_page], pages.collect(&:id).sort
-    assert_equal total_pages, pages.total_pages
+    assert_equal all_pages_ids.size, pages.total_entries
+    assert_equal all_pages_ids[0, per_page], pages.collect(&:id).sort
+    assert_equal total_pagination_pages, pages.total_pages
   end
 end
