cca71d9fd78a00759ea4f347fd73c23b8856d4be
pietro <pietro@riseup.net>
2009-08-17 13:35:32 -0700

    Don't delete message when flagged by moderator.

--- mods/moderation/app/controllers/yucky_controller.rb
+++ mods/moderation/app/controllers/yucky_controller.rb
@@ -50,7 +50,6 @@ class YuckyController < ApplicationController
   end
 
   def add_chat_message
-    @rateable.update_attribute(:deleted_at, Time.now) if current_user.moderator?
     summary = @rateable.content
     date = @rateable.created_at
     url = "/chat/archive/"
