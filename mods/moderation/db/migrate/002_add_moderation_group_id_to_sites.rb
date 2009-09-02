e4c2f0343377705eab33dec2629eadfaaeeb5204
Azul <azul@riseup.net>
2009-09-02 13:35:07 +0200

    CUSTOM: still hacking around moderation migration.

--- mods/moderation/db/migrate/002_add_moderation_group_id_to_sites.rb
+++ mods/moderation/db/migrate/002_add_moderation_group_id_to_sites.rb
@@ -1,6 +1,6 @@
 class AddModerationGroupIdToSites < ActiveRecord::Migration
   def self.up
-#    add_column :sites, :moderation_group_id, :integer
+    add_column :sites, :moderation_group_id, :integer
   end
 
   def self.down
