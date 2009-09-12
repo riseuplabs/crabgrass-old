79a2395decadc131ef323e5e7482b55c8f824d61
Elijah Saxon <elijah@riseup.net>
2009-09-11 18:41:35 -0700

    make "sanitize activity" migration not bomb out if the activity does not refer to a valid post.

--- db/migrate/20090911183629_fix_unsanitized_activity_messages.rb
+++ db/migrate/20090911183629_fix_unsanitized_activity_messages.rb
@@ -1,11 +1,15 @@
 class FixUnsanitizedActivityMessages < ActiveRecord::Migration
   def self.up
     MessageWallActivity.paginated_each do |activity|
-      activity.update_attribute(:extra, {:type => activity.extra[:type], :snippet => GreenCloth.new(Post.find(activity.related_id).body[0..140], 'page', [:lite_mode]).to_html})
+      if post = Post.find_by_id(activity.related_id)
+        activity.update_attribute(:extra, {:type => activity.extra[:type], :snippet => GreenCloth.new(post.body[0..140], 'page', [:lite_mode]).to_html})
+      end
     end
 
     PrivatePostActivity.paginated_each do |activity|
-      activity.update_attribute(:extra, GreenCloth.new(Post.find(activity.related_id).body[0..140], 'page', [:lite_mode]).to_html)
+      if post = Post.find_by_id(activity.related_id)
+        activity.update_attribute(:extra, GreenCloth.new(post.body[0..140], 'page', [:lite_mode]).to_html)
+      end
     end    
   end
 
