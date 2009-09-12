ba44191d39b8a87e0cce5a3b6638c6c220b2537a
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-09-11 21:31:58 -0400

    replace I18n default_exception_handler with a custom handler that does everything the same, but will try to fallback to english translations when the required translation is missing.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -3,3 +3,19 @@
 # All files from config/locales/*.rb,yml are added automatically.
 I18n.load_path <<  Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')]
 I18n.default_locale = :en
+I18n.exception_handler = :crabgrass_i18n_exception_handler
+
+def crabgrass_i18n_exception_handler(exception, locale, key, options)
+  # see i18n.rb in activesupport gem
+  # for the default I18n exception_handler
+  if I18n::MissingTranslationData === exception
+    if locale == :en
+      return exception.message
+    else
+      options[:locale] = :en
+      return I18n.translate(key, options)
+    end
+  end
+
+  raise exception
+end
