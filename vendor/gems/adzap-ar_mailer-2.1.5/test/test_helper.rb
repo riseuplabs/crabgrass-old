8b62f7619f9897c6c4fc1ded07c81dabe48aeda2
Álvaro Gil <zevarito@gmail.com>
2009-10-06 22:50:01 -0200

    Revert "frozen adzap-ar_mailer-2.1.5"
    This reverts commit 048100c667b229b3fe77e610d0d7d62ca0a45e7f.

--- vendor/gems/adzap-ar_mailer-2.1.5/test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require 'rubygems'
-require 'active_support'
-require 'test/resources/action_mailer'
-require 'minitest/autorun'
-require 'mocha'
-
-require 'action_mailer/ar_mailer'
-require 'action_mailer/ar_sendmail'
