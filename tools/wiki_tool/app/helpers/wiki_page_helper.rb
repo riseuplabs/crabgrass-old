0d2fa4d82c299d297651fb0f5609157b47d463d5
Juozas Gaigalas <juozasgaigalas@gmail.com>
2009-10-07 22:02:20 -0700

    fixes #1321 - don't show wiki section edit links for those sections that can't be parsed out

--- tools/wiki_tool/app/helpers/wiki_page_helper.rb
+++ tools/wiki_tool/app/helpers/wiki_page_helper.rb
@@ -43,6 +43,7 @@ module WikiPageHelper
     doc = Hpricot(html)
     doc.search('h1 a.anchor, h2 a.anchor, h3 a.anchor, h4 a.anchor').each do |heading_el|
       section = heading_el['href'].sub(/^.*#/, '')
+      next unless wiki.all_sections.include? section
 
       link_opts = {:url => page_url(@page, :action => 'edit', :section => section), :method => 'get'}
       if show_inline_editor?
