29752598b794b290c6abd4d4bda02ff272584619
k clair <kclair@debian.(none)>
2009-10-08 07:42:22 -0400

    removing migration file

--- db/migrate/20091008111723_moderation_to_version_5.rb
+++
@@ -1,9 +0,0 @@
-class ModerationToVersion5 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["moderation"].migrate(5)
-  end
-
-  def self.down
-    Engines.plugins["moderation"].migrate(3)
-  end
-end
