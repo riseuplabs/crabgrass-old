ceeda29af3a9fc147fd4abb50af6e2f66b3d03cc
Álvaro Gil <zevarito@gmail.com>
2009-10-09 15:04:00 -0200

    move some user preference tests to the right place

--- test/unit/user_setting_test.rb
+++ test/unit/user_setting_test.rb
@@ -1,35 +1,4 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class UserSettingTest < ActiveSupport::TestCase
-  def test_ensure_values_in_receive_notifications 
-    user = User.make
-
-    user.receive_notifications = nil
-    user.save!
-    assert_equal nil, user.receive_notifications
-
-    user.receive_notifications = true
-    user.save!
-    assert_equal nil, user.receive_notifications
-
-    user.receive_notifications = false
-    user.save!
-    assert_equal nil, user.receive_notifications
-
-    user.receive_notifications = "Any" 
-    user.save!
-    assert_equal nil, user.receive_notifications
-
-    user.receive_notifications = "Digest" 
-    user.save!
-    assert_equal "Digest", user.receive_notifications
-
-    user.receive_notifications = "Single" 
-    user.save!
-    assert_equal "Single", user.receive_notifications
-
-    user.receive_notifications = "" 
-    user.save!
-    assert_equal nil, user.receive_notifications
-  end
 end
