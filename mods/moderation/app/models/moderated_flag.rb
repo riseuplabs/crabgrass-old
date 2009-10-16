9200fa7bf0bafb227e30238f15f2e4d0946803b3
Azul <azul@riseup.net>
2009-10-16 13:19:52 +0200

    cleaning up moderated flag a bit.

--- mods/moderation/app/models/moderated_flag.rb
+++ mods/moderation/app/models/moderated_flag.rb
@@ -8,50 +8,36 @@ class ModeratedFlag < ActiveRecord::Base
   belongs_to :user
 
   def add(options={})
-    self.update_attribute(:reason_flagged, options[:reason]) if options[:reason]
-    self.update_attribute(:comment, options[:comment]) if options[:comment]
+    update_attribute(:reason_flagged, options[:reason]) if options[:reason]
+    update_attribute(:comment, options[:comment]) if options[:comment]
     self.save!
   end
 
   def self.trash_all_by_foreign_id(foreign_id)
-    self.update_all('deleted_at=now()',"foreign_id=#{foreign_id}")
+    update_all('deleted_at=now()',"foreign_id=#{foreign_id}")
   end
 
   def user_login
-    return "Unknown" if self.user.nil?
-    return self.user.login
+    return "Unknown" if user.nil?
+    return user.login
   end
 
   ### don't these seem awfully repetitious? ;-)
   def approve
-    self.foreign.update_attribute(:vetted, true)
-    ModeratedFlag.approve_all(self.foreign_id, self.type)
+    foreign.update_attribute(:vetted, true)
+    update_all_flags('vetted_at=now()')
   end
 
   def trash
-    self.foreign.delete
-    ModeratedFlag.trash_all(self.foreign_id, self.type)
+    foreign.delete
+    update_all_flags('deleted_at=now()')
   end
 
   def undelete
-    self.foreign.undelete
-    ModeratedFlag.undelete_all(self.foreign_id, self.type)
+    foreign.undelete
+    update_all_flags('deleted_at=NULL')
   end
   ### end awfully repetitious
-
-  ### oh these also seem repetitious
-  def self.approve_all(foreign_id, type)
-    self.update_all('vetted_at=now()',"foreign_id=#{foreign_id} and type='#{type}'")
-  end
-
-  def self.trash_all(foreign_id, type)
-    self.update_all('deleted_at=now()',"foreign_id=#{foreign_id} and type='#{type}'")
-  end
-
-  def self.undelete_all(foreign_id, type)
-    self.update_all("deleted_at=NULL","foreign_id=#{foreign_id} and type='#{type}'")
-  end
-  ### end also repetitious
 
   def self.display_flags(page, view)
     if view == 'new'
@@ -72,5 +58,11 @@ class ModeratedFlag < ActiveRecord::Base
   named_scope :by_user, lambda {|user|
     { :conditions => ['user_id = ?', user.id] }
   }
+
+  protected
+
+  def update_all_flags(update_string)
+    ModeratedFlag.update_all(update_string, "foreign_id=#{foreign_id} and type='#{type}'")
+  end
 
 end
