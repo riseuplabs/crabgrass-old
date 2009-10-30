a24ade83a42c473d05a0bc886b3a6239ea897da3
Azul <azul@riseup.net>
2009-10-30 20:54:37 +0100

    fixes #1385 - foreign_id not foreign key in page_flag_extension.

--- mods/moderation/app/models/page_flag_extension.rb
+++ mods/moderation/app/models/page_flag_extension.rb
@@ -1,9 +1,9 @@
 module PageFlagExtension
   def self.add_to_class_definition
     lambda do
-      has_many :moderated_pages, :foreign_key => 'foreign_key', :dependent => :destroy
+      has_many :moderated_pages, :foreign_key => 'foreign_id', :dependent => :destroy
     end
   end
-  module InstanceMethods 
+  module InstanceMethods
   end
 end
