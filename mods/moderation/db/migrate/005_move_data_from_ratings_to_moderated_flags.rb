1515c222d1bbfdaffd46f603fce5eb73cedc423f
K. Clair <kclair@kclair-desktop.(none)>
2009-11-05 14:45:58 -0500

    oops one more small fix for moderation migration :)

--- mods/moderation/db/migrate/005_move_data_from_ratings_to_moderated_flags.rb
+++ mods/moderation/db/migrate/005_move_data_from_ratings_to_moderated_flags.rb
@@ -56,7 +56,7 @@ class MoveDataFromRatingsToModeratedFlags < ActiveRecord::Migration
     all_flagged.each do |f|
       next unless f.user_id
       type = 'Page' if f.foreign.is_a?(Page)
-      type = 'DiscussionPost' if f.foreign.is_a?(Post)
+      type = 'Post' if f.foreign.is_a?(Post)
       options = {:rateable_id => f.foreign_id, :rateable_type => type, :user_id => f.user_id, :created_at => f.created_at, :rating => -100 }
       Rating.create!(options)
     end
