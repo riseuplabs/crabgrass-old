02ecaf15791daad17a6451159faaba84a01500ce
Juozas Gaigalas <juozas@hobo23.(none)>
2009-11-15 16:34:12 -0800

    use culerity after all

---
+++ config/environments/culerity_development.rb
@@ -0,0 +1,29 @@
+config.cache_classes = false
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
+config.action_view.cache_template_loading            = false
+
+# Disable request forgery protection in test environment
+config.action_controller.allow_forgery_protection    = false
+
+# Tell Action Mailer not to deliver emails to the real world.
+# The :test delivery method accumulates sent emails in the
+# ActionMailer::Base.deliveries array.
+config.action_mailer.delivery_method = :test
+
+
+ASSET_PRIVATE_STORAGE = "#{RAILS_ROOT}/tmp/private_assets"
+ASSET_PUBLIC_STORAGE  = "#{RAILS_ROOT}/tmp/public_assets"
+
+MIN_PASSWORD_STRENGTH = 0
+
+# however, rails engines are way too verbose, so set engines logging to info:
+if defined? Engines
+  Engines.logger = ActiveSupport::BufferedLogger.new(config.log_path)
+  Engines.logger.level = Logger::INFO
+end
