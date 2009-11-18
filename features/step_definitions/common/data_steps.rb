3b63f19ae0b4cd90c64c9ac68b43ac322aae6f0d
Juozas Gaigalas <juozas@hobo23.(none)>
2009-11-18 01:24:50 -0800

    auth cucumber steps from 1st persom perspective

--- features/step_definitions/common/data_steps.rb
+++ features/step_definitions/common/data_steps.rb
@@ -2,6 +2,11 @@ Given /^#{capture_model} is a member of #{capture_model}$/ do |user, group|
   model(group).add_user!(model(user))
 end
 
+# this could be included in the previous step, but it corrupts up cucumber output a bit
+Given /^I am a member of #{capture_model}$/ do |group|
+  Given "user: \"me\" is a member of #{group}"
+end
+
 Given /^#{capture_model} has (\d+) members$/ do |group, count|
   group = model(group)
   count.to_i.times do
