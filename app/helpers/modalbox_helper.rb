1695466f2ec09c3ed5551a515f2dd1426bc7ded9
Juozas Gaigalas <juozas@hobo23.(none)>
2009-11-19 14:32:26 -0800

    made changes to modalbox.confirm to customize the button names (:ok and :cancel options) and the test

--- app/helpers/modalbox_helper.rb
+++ app/helpers/modalbox_helper.rb
@@ -182,7 +182,7 @@ module ModalboxHelper
         ok = options[:ok] || I18n.t(:ok_button)
         cancel = options[:cancel] || I18n.t(:cancel_button)
         link_to_function(name,
-              %[Modalbox.confirm("#{message}", {method:"#{method}", action:"#{action}", token:"#{token}", title:"#{name}", ok: "#{ok}", cancel: "#{cancel}"})],
+              %[Modalbox.confirm("#{message}", {method:"#{method}", action:"#{action}", token:"#{token}", title:"#{name}", ok:"#{ok}", cancel:"#{cancel}"})],
               html_options)
       else
         link_to_without_confirm(name, options, html_options)
