372259c72fcae7493274b98d7400e987a9adf20f
Juozas Gaigalas <juozas@hobo23.(none)>
2009-11-30 01:57:05 -0800

    mailer cucumber steps now checks only the emails addressed to the current user

--- features/step_definitions/common/mailer_steps.rb
+++ features/step_definitions/common/mailer_steps.rb
@@ -4,9 +4,13 @@ Then /^I should receive an email with (\w+)(:|\s+containing) "([^\"]*)"$/ do |fi
   # equal_op true means option (a)
   equal_op = (op == ":")
 
+  user = model("I")
+
   found_matching = false
   values = []
   delivered_emails.each do |email|
+    # only check emails addressed to this user
+    next unless email.to.include?(user.email)
     # email.send(:subject)
     value = email.send(field.downcase.to_sym)
     values << value
@@ -21,7 +25,7 @@ Then /^I should receive an email with (\w+)(:|\s+containing) "([^\"]*)"$/ do |fi
   end
 
   values = values.join("\n")
-  assert found_matching, "Expected an email to have #{field}#{op} \"#{text}\". Instead found #{field.pluralize}:\n#{values}\n"
+  assert found_matching, "Expected an email to <#{user.email}> to have #{field}#{op} \"#{text}\". Instead found #{field.pluralize}:\n#{values}\n"
 end
 
 Then /^I should receive an email with (\w+)(:|\s+containing) (.*) url$/ do |field, op, page_name|
