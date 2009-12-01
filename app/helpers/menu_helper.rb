d0b5e2d8dc4e477e54deabbb5ace7743967f9f83
zbb <pedro.visintin@gmail.com>
2009-12-01 15:29:53 -0200

    header updated

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -33,6 +33,90 @@ module MenuHelper
     render :partial => 'layouts/menu/'+partial, :locals => locals
   end
 
+  ## *NEWUI
+  ##
+  ## MENUS
+  ##
+  def menu(id, label, url, options={})
+    content_tag(:li,
+      link_to_active(label.upcase, url, options[:active]),
+      :class => [(options[:active] && 'active')].combine,
+      :id => id
+    )
+  end
+
+  def home_option
+    menu(
+      'menu_home',
+      I18n.t(:menu_home),
+      '/',
+      :active => @active_tab == :home
+    )
+  end
+
+  def me_option
+    menu(
+      "menu_me",
+      I18n.t(:menu_me),
+      "/me/dashboard",
+      :active => @active_tab == :me,
+      :menu_items => menu_items('me')
+    )
+  end
+
+  def people_option
+    menu(
+      "menu_people",
+      I18n.t(:menu_people),
+      people_directory_url(:friends),
+      :active => @active_tab == :people,
+      :menu_items => menu_items('boxes', {
+        :entities => current_user.friends.most_active,
+        :heading  => I18n.t(:my_contacts),
+        :see_all_url => people_directory_url(:friends),
+        :submenu => 'people'
+      })
+    )
+  end
+
+  def groups_option
+    menu(
+      "menu_groups",
+      I18n.t(:menu_groups),
+      group_directory_url,
+      :active => @active_tab == :groups,
+      :menu_items => menu_items('boxes', {
+        :entities => current_user.primary_groups.most_active,
+        :heading => I18n.t(:my_groups),
+        :see_all_url => group_directory_url(:action => 'my'),
+        :submenu => 'groups'
+      })
+    )
+  end
+
+  def networks_option
+    menu(
+      "menu_networks",
+      I18n.t(:menu_networks),
+      network_directory_url,
+      :active => @active_tab == :networks,
+      :menu_items => menu_items('boxes', {
+        :entities => current_user.primary_networks.most_active,
+        :heading => I18n.t(:my_networks),
+        :see_all_url => network_directory_url(:action => 'my'),
+        :submenu => 'networks'
+      })
+    )
+  end
+
+  def chat_option
+    menu "menu_chat", I18n.t(:menu_chat), '/chat', :active => @active_tab == :chat
+  end
+
+  def admin_option
+    menu "menu_admin", I18n.t(:menu_admin), '/admin', :active => @active_tab == :admin
+  end
+
   ##
   ## MENUS
   ##
