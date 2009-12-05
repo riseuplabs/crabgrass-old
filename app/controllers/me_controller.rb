e45eaaa35d437fb4c674faff5a71ff851be4f17a
Azul <azul@riseup.net>
2009-12-05 19:42:27 +0100

    restful me controller - yay!
    only does :show, :edit, :update.
    no more me/:action/:id route for me !

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,18 +1,19 @@
 class MeController < Me::BaseController
 
-  def index
+  def show
     flash.keep
     redirect_to :controller => '/me/dashboard'
   end
 
   def edit
-    if request.post?
-      if @user.update_attributes(params[:user])
-        flash_message :success => 'Your profile was successfully updated.'
-        redirect_to me_url(:action => 'edit')
-      else
-        flash_message_now :object => @user
-      end
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash_message :success => 'Your profile was successfully updated.'
+      redirect_to edit_me_url
+    else
+      flash_message_now :object => @user
     end
   end
 
@@ -20,7 +21,7 @@ class MeController < Me::BaseController
 
   def context
     super
-    unless ['show','index'].include?(params[:action])
+    unless ['show'].include?(params[:action])
       # url_for is used here instead of me_url so we can include the *path in the link
       # (it might be a bug in me_url that this is not included, or it might be a bug in url_for
       # that it is. regardless, we want it.)
