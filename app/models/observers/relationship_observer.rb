464c467d17a9527038b24f9739234183b6049bdb
Juozas Gaigalas <juozas@hobo23.(none)>
2009-12-21 14:27:52 -0800

    create a discussion for Relationship's between users using RelationshipObserver instead of alias_method_chain

--- app/models/observers/relationship_observer.rb
+++ app/models/observers/relationship_observer.rb
@@ -1,5 +1,19 @@
 class RelationshipObserver < ActiveRecord::Observer
 
+  def before_create(relationship)
+    # Relationship's are created in pairs
+    mirror_twin = Relationship.find_by_user_id_and_contact_id(relationship.contact_id, relationship.user_id)
+
+    # the first one will have no mirror_twin yet and will build a new Discussion
+    # then that Relationship will get saved
+    # the second Relationship int the pair will copy the discussion from the first one
+    if mirror_twin and mirror_twin.discussion
+      relationship.discussion_id = mirror_twin.discussion.id
+    else
+      relationship.create_discussion
+    end
+  end
+
   def after_create(relationship)
     if relationship.type == "Friendship"
       if activity = FriendActivity.find_twin(relationship.user, relationship.contact)
