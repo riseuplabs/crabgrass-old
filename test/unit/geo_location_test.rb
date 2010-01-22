277dab6e8a7f3593c86cbea566c5f23e4e9efece
K. Clair <kclair@kclair-desktop.(none)>
2010-01-22 15:21:51 -0500

    fixes locations unit test failures.

--- test/unit/geo_location_test.rb
+++ test/unit/geo_location_test.rb
@@ -3,13 +3,8 @@ require 'test_helper'
 class GeoLocationTest < ActiveSupport::TestCase
 
   def test_country_id_required
-    geo_location = GeoLocation.new(:profile_id => 4)
+    geo_location = GeoLocation.new()
     assert !geo_location.save, "Saved GeoLocation without country id."
-  end
-
-  def test_profile_id_required
-    geo_location = GeoLocation.new(:country_id => 1)
-    assert !geo_location.save, "Saved GeoLocation without profile id."
   end
 
 end
