0f86b066dd6d7f431030800baeb27572a60c2322
Juozas Gaigalas <juozasgaigalas@gmail.com>
2010-01-22 02:27:48 -0800

    requests tab with action bar

--- app/controllers/me/requests_controller.rb
+++ app/controllers/me/requests_controller.rb
@@ -12,7 +12,21 @@
 
 class Me::RequestsController < Me::BaseController
 
-  helper 'requests'
+  helper 'requests', 'action_bar'
+
+  # pending requests
+  def index
+    @requests = Request.created_by(current_user).paginate(page_params)
+  end
+
+  def approved
+    render :action => :index
+  end
+
+  def rejected
+    render :action => :index
+  end
+
 
   def from_me
     @requests = Request.created_by(current_user).having_state(params[:state]).by_created_at.paginate(:page => params[:page])
@@ -24,20 +38,14 @@ class Me::RequestsController < Me::BaseController
 
   protected
 
-  before_filter :default_state
-  def default_state
-    params[:state] ||= 'pending'
-  end
-
   def context
     super
     me_context('small')
-    #add_context 'requests', url_for(:controller => 'me/requests', :action => nil)
-    #if action?(:to_me)
-    #  add_context I18n.t(:requests_to_me), url_for(:controller => '/me/requests', :action => 'to_me')
-    #elsif action?(:from_me)
-    #  add_context I18n.t(:requests_from_me), url_for(:controller => '/me/requests', :action => 'from_me')
-    #end
+  end
+
+
+  def page_params(default_page = nil, per_page = nil)
+    {:page => params[:page] || default_page, :per_page => nil}
   end
 
 end
