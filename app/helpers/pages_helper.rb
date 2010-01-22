b0cac23da9bd09ced5cdc70cbbf856db0a263975
Juozas Gaigalas <juozasgaigalas@gmail.com>
2010-01-22 02:26:10 -0800

    new semantics for action_bar helper to make it reusable between pages, messages and requests. see action_bar_helper.rb, pages_helper.rb and my_work.html.haml for usage.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,28 +1,46 @@
 module PagesHelper
-
-  #
-  # some definitions for the action bar.
-  #
-  def selections
-    [:all, :none, :unread]
+  def my_work_view_settings
+    [
+      {:name => :my_work, :translation => :view_my_work_pages_option},
+      {:name => :watched, :translation => :view_watched_pages_option},
+      {:name => :editor, :translation => :view_editor_pages_option},
+      {:name => :owner, :translation => :view_owner_pages_option},
+      {:name => :unread, :translation => :view_unread_pages_option}
+    ]
   end
 
-  def selectors
-    { :all => ".page_check",
-      :unread => "section.pages_info.unread .page_check",
-      :none => ''
-    }
+  def all_view_settings
+   [
+     {:name => :public, :translation => :public},
+     {:name => :networks, :translation => :networks},
+     {:name => :groups, :translation => :groups}
+    ]
   end
 
-  def marks
-    [:read, :unread, :watched, :unwatched]
-  end
-
-  def views
+  def view_settings
     if @tab == :my_work
-      [:my_work, :watched, :editor, :owner, :unread]
+      my_work_view_settings
     elsif @tab == :all
-      [:public, :networks, :groups]
+      all_view_settings
     end
+  end
+
+  def action_bar_settings
+    { :select =>
+            [ {:name => :all,
+               :translation => :select_all,
+               :function => checkboxes_subset_function(".page_check", ".page_check")},
+              {:name => :none,
+               :translation => :select_none,
+               :function => checkboxes_subset_function(".page_check", "")},
+              {:name => :unread,
+               :translation => :select_none,
+               :function => checkboxes_subset_function(".page_check", "section.pages_info.unread .page_check")}],
+      :mark =>
+            [ {:name => :read, :translation => :read},
+              {:name => :unread, :translation => :unread},
+              {:name => :watched, :translation => :watched},
+              {:name => :unwatched, :translation => :unwatched}],
+      :view => view_options }
   end
 end
