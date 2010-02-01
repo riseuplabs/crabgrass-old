9dec431f3ee4bd43da172cf563929de8b120d152
Azul <azul@riseup.net>
2010-02-01 14:16:37 +0100

    tests follow messages into me namespace...

--- test/functional/me/message_posts_controller_test.rb
+++ test/functional/me/message_posts_controller_test.rb
@@ -1,6 +1,6 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
-class MessagePostsControllerTest < ActionController::TestCase
+class Me::MessagePostsControllerTest < ActionController::TestCase
   fixtures :users, :relationships, :discussions
 
 
