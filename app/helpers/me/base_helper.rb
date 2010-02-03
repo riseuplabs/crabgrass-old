9ef696b23b7fdd11eb9e0d07db6542fa2b7fb884
K. Clair <kclair@kclair-desktop.(none)>
2010-02-03 15:18:53 -0500

    say box / status posts work

--- app/helpers/me/base_helper.rb
+++ app/helpers/me/base_helper.rb
@@ -7,13 +7,4 @@ module Me::BaseHelper
     )
   end
 
-  def say_box_onblur
-    onblur = remote_function(
-      :url => {:controller => '/me/messages', :action => 'public'},
-      :with => "'post[body]='+value",
-      :loading => show_spinner('say'),
-      :complete => hide_spinner('say')
-    )
-  end
-
 end
