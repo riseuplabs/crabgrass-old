50ec48bd4e87cfd0b0851e8073d99dc5712a9605
Juozas Gaigalas <juozasgaigalas@gmail.com>
2010-02-05 18:58:54 -0800

    fixed couple of avatar problems: 1) always expire 'big' avatar cache 2) 'xlarge' avatar is still used by a lot of crabgrass code, so I re-added the definition for 'xlarge' (96x96)

--- app/sweepers/avatar_sweeper.rb
+++ app/sweepers/avatar_sweeper.rb
@@ -2,7 +2,7 @@ class AvatarSweeper < ActionController::Caching::Sweeper
   observe Avatar
 
   def after_save(record)
-    for size in %w(xsmall small medium large xlarge)
+    for size in %w(xsmall small medium large xlarge big)
       expire_page :controller => 'static', :action => 'avatar', :id => record.id, :size => size
     end
   end
