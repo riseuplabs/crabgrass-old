ad38edf536455951fb1741d7af77e2fe7f350bac
Juozas Gaigalas <juozasgaigalas@gmail.com>
2010-02-08 19:03:32 -0800

    help(symbol) helper understand new translation handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -327,9 +327,16 @@ module ApplicationHelper
   # returns the related help string, but only if it is translated.
   def help(symbol)
     symbol = "#{symbol}_help".to_sym
-    text = I18n.t(symbol)
-    # return nil if I18n.t says translation is missing
-    text =~ /translation missing/ ? nil : text
+    text = nil
+    begin
+      text = I18n.t(symbol)
+    rescue I18n::MissingTranslationData
+      # this error is only raised in dev/test mode when translation is missing
+      return nil
+    end
+
+    # return nil if I18n.t can't find the translation (in production mode) and has to humanize it
+    text == symbol.to_s.humanize ? nil : text
   end
 
   def debug_permissions
