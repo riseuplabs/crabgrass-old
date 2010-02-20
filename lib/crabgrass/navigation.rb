2e434870aab5690b159af40c9b5a80546528b508
Azul <azul@riseup.net>
2010-02-20 23:17:23 +0100

    Navigation based on RubyTree - breaks the feature
    now we can do things like @nav.parent. But the feature needs to be
    adopted.

--- lib/crabgrass/navigation.rb
+++ lib/crabgrass/navigation.rb
@@ -1,13 +1,28 @@
-class Navigation
+require "yaml"
+require "tree"
 
-  require "yaml"
-  
-  def initialize
-    @structure = YAML.load_file([RAILS_ROOT, 'config', 'nav_structure.yml'].join('/'))
-  end 
+class Navigation < Tree::TreeNode
 
-  def structure
-    @structure
+  def initialize(*args)
+    case args.count
+    when 0..1
+      filename = args[0] || [RAILS_ROOT, 'config', 'nav_structure.yml'].join('/')
+      structure = YAML.load_file filename
+      initialize('/', structure)
+    when 2
+      name = args[0]
+      structure = args[1]
+      super(name, {})
+      structure.each_pair do |key, value|
+        if value.is_a? Hash
+          add Navigation.new(key, value)
+        elsif !value.nil?
+          content[key] = value
+        end
+      end
+    else
+      raise ArgumentError.new("wrong number of arguments (#{args.count} for 2)")
+    end
   end
 
 end
