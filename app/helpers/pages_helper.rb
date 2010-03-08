6547ef74951e2b75232d05df7179c60dd0e8abb4
K. Clair <kclair@kclair-laptop.(none)>
2010-03-08 15:33:38 -0500

    toward #1902.  added ability to remove pagination from within page list because it was the easiest way to move the pagination on my_work. still not able to get info-box and pagination to play nicely on the same line, also italics need to be added.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -36,7 +36,7 @@ module PagesHelper
 
   def my_work_pages_list
     render :partial => 'pages/list',
-      :locals => {:checkable => true, :with_notice => true, :full_width => true}
+      :locals => {:checkable => true, :with_notice => true, :full_width => true, :no_top_pagination => true}
   end
 
   def my_work_settings
