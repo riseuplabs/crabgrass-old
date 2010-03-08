5b61cf00c5602d2c0358e59cf3fe7032e085db17
Juozas Gaigalas <juozasgaigalas@gmail.com>
2010-03-08 15:39:46 -0800

    fixes #1894 - removed unread activity for private messages

--- app/models/observers/relationship_observer.rb
+++ app/models/observers/relationship_observer.rb
@@ -25,15 +25,5 @@ class RelationshipObserver < ActiveRecord::Observer
     end
   end
 
-  # i think perhaps it is unneccesary to create a new UnreadActivity each time
-  # a new private post is created, since there will already be a private post
-  # activity, and we will update the UnreadActivity when the user views the post.
-
-  def after_save(relationship)
-    if relationship.unread_count_changed?
-      UnreadActivity.create(:user => relationship.user, :author => relationship.contact)
-    end
-  end
-
 end
 
