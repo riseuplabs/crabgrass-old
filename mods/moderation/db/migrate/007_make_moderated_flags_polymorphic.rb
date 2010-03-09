8a8e0b128f4fff0dd2848f972c2f542dcd83b651
K. Clair <kclair@kclair-desktop.(none)>
2010-03-08 20:09:51 -0500

    fixes #1882. i think after these migrations are run then the ModeratedFlags subclasses (ModeratedPage, ModeratedPost) need to go away.

--- mods/moderation/db/migrate/007_make_moderated_flags_polymorphic.rb
+++ mods/moderation/db/migrate/007_make_moderated_flags_polymorphic.rb
@@ -8,11 +8,13 @@ class MakeModeratedFlagsPolymorphic < ActiveRecord::Migration
       # This can't be done with update all because we can't set flagged_type to the
       # specific subclasses.
       next unless Page.exists?(mpage.flagged_id)
-      mpage.flagged=Page.find(mpage.flagged_id)
+      #mpage.flagged=Page.find(mpage.flagged_id)  <-- this is for some reason not updating flagged_type
+      mpage.update_attributes!(:flagged_type => 'Page')
     end
     ModeratedFlag.find_all_by_type("ModeratedPost").each do |mpage|
       next unless Post.exists?(mpage.flagged_id)
-      mpage.flagged=Post.find(mpage.flagged_id)
+      #mpage.flagged=Post.find(mpage.flagged_id)
+      mpage.update_attributes!(:flagged_type => 'Post')
     end
   end
 
