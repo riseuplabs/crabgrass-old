20c71c27f5e8d01289820ba8ee0f1e2aa9ef2040
K. Clair <kclair@kclair-desktop.(none)>
2010-04-06 09:11:10 -0400

    this should fix the cucumber test failures. consolidated filter by location tests

--- features/step_definitions/common/location_steps.rb
+++ features/step_definitions/common/location_steps.rb
@@ -1,6 +1,6 @@
 Given /^geo data exists$/ do
-  Given "a geo_country exists with name: \"United States\", code: \"US\""
-  Given "a geo_country exists with name: \"Netherlands\", code: \"NL\""
+  Given "a geo_country: \"US\" exists with name: \"United States\", code: \"US\""
+  Given "a geo_country: \"NL\" exists with name: \"Netherlands\", code: \"NL\""
   Given "a geo_admin_code exists with name: \"Philadelphia\", geo_country_id: 1, admin1_code: \"PA\""
   Given "a geo_admin_code exists with name: \"Washington\", geo_country_id: 1, admin1_code: \"WA\""
   Given "a geo_admin_code exists with name: \"Utrecht\", geo_country_id: 2, admin1_code: \"09\""
