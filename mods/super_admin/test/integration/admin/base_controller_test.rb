df55a82dd721da1c6d4c22d78dc7845a41bec825
Azul <azul@riseup.net>
2010-04-19 11:51:28 +0200

    using skip_if in superadmin integration test
    Just to make sure skip_if also works for integration tests

--- mods/super_admin/test/integration/admin/base_controller_test.rb
+++ mods/super_admin/test/integration/admin/base_controller_test.rb
@@ -1,45 +1,47 @@
 require "#{File.dirname(__FILE__)}/../../test_helper"
 
-class Admin::BaseTest < ActionController::IntegrationTest
-  skip_if :mod_disabled_or_migrations_fail?
+class SuperAdmin::Admin
+  class BaseTest < ActionController::IntegrationTest
+    skip_if :mod_disabled?
 
-  fixtures :users, :memberships, :groups, :sites
+    fixtures :users, :memberships, :groups, :sites
 
-  def setup
-    enable_site_testing
-  end
+    def setup
+      enable_site_testing
+    end
 
-  def test_become_and_return
-    host! 'test.host'
-    # has to be account/login instead of '/' (root)
-    # because only account controller has CSRF protection enabled
-    visit '/account/login'
-    fill_in "Login name", :with => 'blue'
-    fill_in "Password", :with => 'blue'
-    click_button "Log in"
-    assert_response :redirect
-    follow_redirect!
-    assert_contain "Logout"
-    assert_select "a[href='/blue']", "Profile"
+    def test_become_and_return
+      host! 'test.host'
+      # has to be account/login instead of '/' (root)
+      # because only account controller has CSRF protection enabled
+      visit '/account/login'
+      fill_in "Login name", :with => 'blue'
+      fill_in "Password", :with => 'blue'
+      click_button "Log in"
+      assert_response :redirect
+      follow_redirect!
+      assert_contain "Logout"
+      assert_select "a[href='/blue']", "Profile"
 
 
-    click_link 'Admin'
-    assert_contain "Super Admin Powers"
+      click_link 'Admin'
+      assert_contain "Super Admin Powers"
 
-    # click_link "edit users"
-    visit '/admin/users'
-    assert_contain "Become"
+      # click_link "edit users"
+      visit '/admin/users'
+      assert_contain "Become"
 
-    click_link 'D'
-    click_link 'Become'
-    follow_redirect!
-    assert_contain "Logout"
-    assert_select "a[href='/dolphin']", "Profile"
-    assert_contain "Admin"
+      click_link 'D'
+      click_link 'Become'
+      follow_redirect!
+      assert_contain "Logout"
+      assert_select "a[href='/dolphin']", "Profile"
+      assert_contain "Admin"
 
-    click_link 'Admin'
-    follow_redirect!
-    assert_contain "Super Admin Powers"
-    assert_select "a[href='/blue']", "Profile"
+      click_link 'Admin'
+      follow_redirect!
+      assert_contain "Super Admin Powers"
+      assert_select "a[href='/blue']", "Profile"
+    end
   end
 end
--- mods/super_admin/test/test_helper.rb
+++ mods/super_admin/test/test_helper.rb
@@ -1,2 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/mod_test_helper')
 Engines::Testing.set_fixture_path
+
+class SuperAdmin < Mod
+  # this is used for namespacing in the tests and storing information that is
+  # relevant for the whole mod - such as migrations.
+end
