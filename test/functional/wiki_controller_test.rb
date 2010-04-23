31bbe18794195832b7b1e40fef43ead7fbda2992
K. Clair <kclair@kclair-desktop.(none)>
2010-04-23 10:56:44 -0400

    added a little coverage for wiki_controler (#2216).

--- test/functional/wiki_controller_test.rb
+++ test/functional/wiki_controller_test.rb
@@ -5,7 +5,7 @@ require 'wiki_controller'
 class WikiController; def rescue_action(e) raise e end; end
 
 class WikiControllerTest < Test::Unit::TestCase
-  fixtures :groups, :pages, :users, :memberships, :sites
+  fixtures :groups, :pages, :users, :memberships, :sites, :profiles, :wikis
 
   def setup
     @controller = WikiController.new
@@ -15,9 +15,32 @@ class WikiControllerTest < Test::Unit::TestCase
 
 # TODO: write tests for this controller
 
+  def test_before_filters
+    # just using the edit action to test the various ways to get the wiki
+    group = groups(:rainbow)
 
-  def test_show
-    assert true
+    get :edit, :wiki_id => 300, :group_id => group.id
+    assert_response :redirect
+
+    login_as :blue
+
+    get :edit, :wiki_id => 300, :group_id => group.id   
+    assert assigns(:wiki)
+
+    # this actually bafflingly assigns both @public and @private 
+    get :edit, :profile_id => 3, :group_id => group.id 
+    assert assigns(:wiki) ## i don't really understand how @wiki is getting assigned here?!?!?!
+    assert assigns(:profile)
+    assert assigns(:private)
+    assert assigns(:public)
+    assert_equal assigns(:wiki), assigns(:public)
+ 
+    ## now that the above has loaded the wiki_id's for the profiles are different
+    ## so we can test retrieving by profile id and wiki id to make sure they match
+    public_wiki = assigns(:public)
+    get :edit, :profile_id => 31, :group_id => group.id, :wiki_id => public_wiki.id
+    assert assigns(:wiki)
+    assert_equal assigns(:wiki), assigns(:public)
   end
 
 end
