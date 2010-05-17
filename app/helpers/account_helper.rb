ea379e2aff0c0a88408d2bc23bd2043f45b47382
Azul <azul@riseup.net>
2010-05-17 12:14:05 +0200

    adopt the more flexible signup redirect to 0.5

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,2 +1,8 @@
 module AccountHelper
+  def signup_url
+    current_site.signup_redirect_url ||
+      url_for :controller => '/account',
+        :action => 'signup',
+        :redirect => params[:redirect]
+  end
 end
