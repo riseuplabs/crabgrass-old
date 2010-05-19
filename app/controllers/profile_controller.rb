riseuplabs/crabgrass-old

commit c4a2fd67825f9fa4e8af561de8e3a3fbc0efebf6

restrict update when moderating page fixes #2207
* removed buggy allow and forbid extensions to Hash.
* use Hash.symbolize_keys.slice / reject instead.

app/controllers/profile_controller.rb
@@ -119,11 +119,11 @@ class ProfileController < ApplicationController
 #      # from saving.
 #      valid_keys = this_params.map do |key, value|
 #        values = ((ignored_keys = ignore[element]) ?
-#                  value.allow(value.keys-ignored_keys).values :
+#                  value.slice(value.keys-ignored_keys).values :
 #                  value.values)
 #        values.map(&:empty?).include?(false) ? key : nil
 #      end.compact
-#      params['profile'][plural] = this_params.allow(valid_keys)
+#      params['profile'][plural] = this_params.slice(valid_keys)
 #    end
 #  end
 

app/models/profile/profile.rb
@@ -188,11 +188,11 @@ class Profile < ActiveRecord::Base
       'phone_numbers'   => ::ProfilePhoneNumber,   'locations' => ::ProfileLocation,
       'email_addresses' => ::ProfileEmailAddress,  'websites'  => ::ProfileWebsite,
       'im_addresses'    => ::ProfileImAddress,     'notes'     => ::ProfileNote,
-      'crypt_keys'      => ::ProfileCryptKey      
+      'crypt_keys'      => ::ProfileCryptKey
     }
 
     profile_params.stringify_keys!
-    params = profile_params.allow(valid_params)
+    params = profile_params.slice valid_params
     params['summary_html'] = nil if params['summary'] == ""
 
     # save nil if value is an empty string:
@@ -210,7 +210,7 @@ class Profile < ActiveRecord::Base
 
     params['photo'] = Asset.build(params.delete('photo')) if params['photo']
     params['video'] = ExternalVideo.new(params.delete('video')) if params['video']
-    
+
     geo_location_options = {
       :geo_country_id => params.delete('country_id'),
       :geo_admin_code_id => params.delete('state_id'),

app/views/pages/_search.rhtml
@@ -13,7 +13,7 @@
   # we use strings for boolean so that we can correctly set defaults
   show_extras ||= 'false'
   show_page_types ||= 'true'
-  url ||= params.allow(:controller, :action, :id)
+  url ||= params.symbolize_keys.slice :controller, :action, :id
   url[:path] = nil if url.is_a?(Hash)
 
   group_id = @path.int_for('group')

lib/extension/core.rb
@@ -208,40 +208,6 @@ class Array
 end
 
 
-class Hash
-  # returns a copy of the hash, limited to the specified keys
-  def allow(*keys)
-    keys = keys.first if keys.first.is_a? Array
-    hsh = {}
-    keys.each do |key|
-      value = self[key] || self[key.to_s] || self[key.to_sym]
-      hsh[key] = value if value
-    end
-    hsh
-  end
-
-  # returns a copy of the hash, without any of the specified keys
-  def forbid(*keys)
-    keys = keys.first if keys.first.is_a? Array
-    hsh = self.clone
-    keys.each do |key|
-      hsh.delete(key); hsh.delete(key.to_s); hsh.delete(key.to_sym)
-    end
-    hsh
-  end
-
-=begin
-  # returns a copy of the hash with symbols
-  def symbolize
-    self.keys.inject({})  { |m, k|
-      m[k.kind_of?(Hash) ? k.symbolize : (k.respond_to?(:to_sym) ? k.to_sym : k)] = ((v = v.to_sym    rescue nil) ||
-                                                                                     (v = v.symbolize rescue nil) || v)
-      m
-    }
-  end
-=end
-end
-
 class Symbol
   # should do the same as sym.to_s.any?. symbols are never empty, hence #=> true
   def any?

mods/moderation/app/controllers/admin/pages_controller.rb
@@ -17,11 +17,12 @@ class Admin::PagesController < Admin::BaseController
     @flagged = Page.paginate_by_path(path_for_view, options)
   end
 
-  # for vetting:       params[:page][:vetted] == true
-  # for hiding:        params[:page][:flow]   == FLOW[:deleted]
-  # for making public: params[:page][:public] == true
+  # for vetting:       params[:page]['vetted'] == true
+  # for hiding:        params[:page]['flow']   == FLOW[:deleted]
+  # for making public: params[:page]['public'] == true
   def update
-    @page.update_attributes(params[:page])
+    page_attrs = params[:page].symbolize_keys.slice :vetted, :flow, :public
+    @page.update_attributes(page_attrs)
     redirect_to :action => 'index', :view => params[:view]
   end
 

commit f6de747c960555f4ee6f6aaf6022b3da2b5c055e

fixing bug introduced in c4a2fd6782 and cleaning up profile controller

app/controllers/profile_controller.rb
@@ -104,29 +104,6 @@ class ProfileController < ApplicationController
   #                       @site.profiles.private)
   #end
 
-#  # removes everything from params that isn't to be included, due to the profile
-#  # settings of the current site.
-#  def apply_settings_to_params!
-#    # values are preset (select field), so we ignore them
-#    ignore = {
-#      'location' => ['location_type'],
-#      'note' => ['note_type']
-#    }
-#    %w(crypt_key email_address location website note im_address
-#       phone_number).each do |element|
-#      next unless (this_params = params['profile'][plural = element.pluralize])
-#      # elements with all fields empty shouldn't be fatal errors that prevent us
-#      # from saving.
-#      valid_keys = this_params.map do |key, value|
-#        values = ((ignored_keys = ignore[element]) ?
-#                  value.slice(value.keys-ignored_keys).values :
-#                  value.values)
-#        values.map(&:empty?).include?(false) ? key : nil
-#      end.compact
-#      params['profile'][plural] = this_params.slice(valid_keys)
-#    end
-#  end
-
   before_filter :setup_layout
   def setup_layout
     if @user

app/models/profile/profile.rb
@@ -192,7 +192,7 @@ class Profile < ActiveRecord::Base
     }
 
     profile_params.stringify_keys!
-    params = profile_params.slice valid_params
+    params = profile_params.reject {|key,| !valid_params.include?(key)}
     params['summary_html'] = nil if params['summary'] == ""
 
     # save nil if value is an empty string: