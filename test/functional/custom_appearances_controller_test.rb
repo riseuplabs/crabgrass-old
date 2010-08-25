142f928281ee17bc3189ae8a816ee41b984b2d55
Elijah Saxon <elijah@riseup.net>
2010-08-25 01:14:14 -0700

    removed all files related to CustomAppearance. soon to be replaced with new theme code. maybe some custom appearance gui code will make a return.
    Signed-off-by: Elijah Saxon <elijah@riseup.net>

--- test/functional/custom_appearances_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CustomAppearancesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  fixtures :sites, :groups, :custom_appearances, :users
-
-end
