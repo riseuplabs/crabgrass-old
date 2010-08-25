e98ce5e17c5f3dac84664a66fbd110231dbf5e8e
Elijah Saxon <elijah@riseup.net>
2010-08-25 01:19:40 -0700

    made a bunch of plugins work better with rails 2.3.8 (mostly moved the task file to avoid warning message)

--- vendor/plugins/acts_as_rateable/init.rb
+++ vendor/plugins/acts_as_rateable/init.rb
@@ -1,3 +1,9 @@
 # Include hook code here
 require 'acts_as_rateable'
 ActiveRecord::Base.send(:include, Juixe::Acts::Rateable)
+
+# require File.dirname(__FILE__) + '/lib/acts_as_rateable'
+# require File.dirname(__FILE__) + '/lib/rating'
+# ActiveRecord::Base.send(:include, Juixe::Acts::Rateable)
+
+
