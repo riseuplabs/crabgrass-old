2077cf04885c402e81881a505ffaea2b938b053b
Azul <azul@riseup.net>
2010-10-16 12:08:24 +0200

    Formy.tabs is gone... not sure if we will use formy(:tabs) now
    * It works for now and fixes the issue when looking at sitehome.
    * Also added test for tabsets to formy
    * I'd like to move formy tabset to unobstrusive js.

--- vendor/plugins/crabgrass_formy/lib/formy.rb
+++ vendor/plugins/crabgrass_formy/lib/formy.rb
@@ -4,9 +4,10 @@ require 'formy/root'
 require 'formy/buffer'
 
 require 'formy/simple_form'
+require 'formy/tabset'
 
 module Formy
-  
+
 #  def self.create(options={})
 #    @@base = f = Form.new(options)
 #    f.open
--- vendor/plugins/crabgrass_formy/test/test.rb
+++ vendor/plugins/crabgrass_formy/test/test.rb
@@ -16,7 +16,7 @@ class Testr
   include Formy::Helper
 
   def run
-    formy(:simple_form) do |f|
+    simple_form = formy(:simple_form) do |f|
       f.row do |r|
         r.label 'display name', 'user_display_name'
         r.input text_field 'user', 'display_name'
@@ -26,6 +26,19 @@ class Testr
         r.input text_field 'user', 'email'
       end
     end
+    tabset = formy(:tabset) do |f|
+      f.tab do |t|
+        t.label 'Tab One'
+        t.show_tab 'tab-one-div'
+        t.selected true
+      end
+      f.tab do |t|
+        t.label 'Tab Two'
+        t.show_tab 'tab-two-div'
+        t.selected false
+      end
+    end
+    simple_form + tabset
   end
 
 end
