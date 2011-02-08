8224e6d20318f60f4ff623a6e65b80ae59c0cc04
k clair <kclair@pickledradish.com>
2011-02-08 10:05:00 -0500

    add deploy info

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :application, "crabgrass"
 set :user, "crabgrass"
 
 set :repository, "git://labs.riseup.net/crabgrass.git"
-set :branch, "munia"
+set :branch, "admin_stats"
 
 deploy_host = "xxxxxx"
 staging_host = "munia.riseup.net"
