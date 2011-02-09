fa09ee2c7be5909022d23cb887a1056590b68693
Azul <azul@riseup.net>
2011-02-09 10:42:46 +0100

    fixing locations_to_kml to exclude entities that can not be found
    we have some entities acting as site limited. Thus their profiles can be found but the entities themselves will return nil.

--- mods/locations_to_kml/app/controllers/locations_controller.rb
+++ mods/locations_to_kml/app/controllers/locations_controller.rb
@@ -4,7 +4,8 @@ class LocationsController
     @places = []
     GeoLocation.find(:all, :conditions => ['geo_place_id IS NOT NULL']).each do |gl|
       next unless profile = Profile.find_by_geo_location_id(gl.id)
-      place = {} 
+      next unless profile.entity
+      place = {}
       place[:name] = profile.entity.try(:display_name) || profile.entity.name
       place[:description] = 'this is the location of entity '+place[:name]+' and can have a link <a href="foo">foo</a>'
       place[:lat] = GeoPlace.find(gl.geo_place_id).latitude
