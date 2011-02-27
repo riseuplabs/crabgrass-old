70aeb169ec65cc617d39eeffe7ae1c63eaf23844
k clair <kclair@pickledradish.com>
2011-02-26 22:49:44 -0500

    remove old mod files

--- mods/locations_to_kml/init.rb
+++
@@ -1,4 +0,0 @@
-Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
-
-Dispatcher.to_prepare do
-end
--- mods/locations_to_kml/test/test_helper.rb
+++
@@ -1,2 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-Engines::Testing.set_fixture_path
