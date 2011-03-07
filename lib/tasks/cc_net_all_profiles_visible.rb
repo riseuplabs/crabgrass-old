1d47303fe6dfd4a743432ee508ea91d55fb735e2
k clair <kclair@pickledradish.com>
2011-03-07 18:00:22 -0500

    only make user profiles visible

--- lib/tasks/cc_net_all_profiles_visible.rb
+++ lib/tasks/cc_net_all_profiles_visible.rb
@@ -1,6 +1,7 @@
 namespace :cg do
   task :cc_net_all_profiles_visible => :environment do
     Profile.find(:all).each do |prof|
+      next unless prof.entity.is_a? User
       prof.update_attribute('may_see', 1)
       prof.save!
     end
