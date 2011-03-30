853d5319923bb99b8a7d9d7f1dc2d8f196012059
k clair <kclair@pickledradish.com>
2011-03-30 10:47:28 -0400

    missing file from last commit - get kml from app/views/map

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -242,10 +242,10 @@ class ApplicationController < ActionController::Base
   def render_kml_for(collection)
     if params[:sort_by] == 'latlong'
       @places = GeoPlace.sort_entities_by_place(collection)
-      render :template => 'locations/index_by_latlong.kml.builder', :layout => false
+      render :template => 'map/index_by_latlong.kml.builder', :layout => false
     else
       @entities = collection
-      render :template => 'locations/index.kml.builder', :layout => false
+      render :template => 'map/index.kml.builder', :layout => false
     end
   end
 
