38643cd00470b780a41032699bbc98bf5254d32f
Azul <azul@riseup.net>
2011-04-05 12:13:53 +0200

    default widgets according to old site home. moved widget yaml out of fixtures

--- lib/tasks/create_default_site_widgets.rb
+++ lib/tasks/create_default_site_widgets.rb
@@ -4,9 +4,11 @@ namespace :cg do
     widgets = load_widgets(ENV['WIDGETS'] || 'old')
     if name == 'ALL'
       Site.all.each do |site|
+        clear_widgets(site) if ENV['CLEAR']
         add_widgets(site, widgets)
       end
     elsif site = Site.find_by_name(name)
+      clear_widgets(site) if ENV['CLEAR']
       add_widgets(site, widgets)
     end
   end
@@ -22,18 +24,31 @@ end
 
 def self.load_widgets(filename)
   filename += '_widgets.yml' unless filename.index('yml')
-  filename = [RAILS_ROOT, 'test', 'fixtures', filename].join('/')
+  filename = [RAILS_ROOT, 'lib', 'tasks', filename].join('/')
   YAML.load_file(filename)
 end
 
+def self.clear_widgets(site)
+  puts "Clearing widgets from site #{site.name}..."
+  return unless profile = get_profile(site)
+  profile.widgets.destroy_all
+end
+
 def self.add_widgets(site, widgets)
+  return unless profile = get_profile(site)
+  puts "Creating widgets for site #{site.name}..."
+  widgets.each do |params|
+    puts "Creating widget #{params['name']}."
+    unless profile.widgets.create(params)
+      puts "... failed - please make sure the widget settings are valid."
+    end
+  end
+end
+
+def self.get_profile(site)
   unless site.network && profile = site.network.profiles.public
     puts "ERROR: site #{site.name} does not have a network with a public profile."
     return
   end
-  puts "Creating widgets for site #{site.name}..."
-  widgets.values.each do |params|
-    puts "Creating widget #{params['name']}."
-    profile.widgets.create(params)
-  end
+  profile
 end
