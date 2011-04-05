8f0055d0daaf1147fe2de8e86395dda66772b876
Azul <azul@riseup.net>
2011-04-05 10:38:58 +0200

    more rake task refactoring

--- lib/tasks/create_default_site_widgets.rb
+++ lib/tasks/create_default_site_widgets.rb
@@ -1,29 +1,37 @@
 namespace :cg do
   task :create_default_site_widgets => :environment do
-    unless name = ENV['SITE']
-      puts 'ERROR: site name required, use SITE=<name> to specify the name or SITE=ALL'
-      exit
-    end
+    name = ensure_site(ENV['SITE'])
+    widgets = load_widgets(ENV['WIDGETS'] || 'old')
     if name == 'ALL'
-      Site.all.each do |s|
-        add_widgets(s)
+      Site.all.each do |site|
+        add_widgets(site, widgets)
       end
-    elsif s = Site.find_by_name(name)
-      add_widgets(s)
+    elsif site = Site.find_by_name(name)
+      add_widgets(site, widgets)
     end
   end
 end
 
-def self.add_widgets(s)
-  unless s.network && profile = s.network.profiles.public
-    puts "ERROR: site #{s.name} does not have a network with a public profile."
-    return
+def self.ensure_site(name)
+  if name.blank?
+    puts 'ERROR: site name required, use SITE=<name> to specify the name or SITE=ALL'
+    exit
   end
-  puts "Creating widgets for site #{s.name}..."
-  filename = (ENV['WIDGETS'] || 'old')
+  name
+end
+
+def self.load_widgets(filename)
   filename += '_widgets.yml' unless filename.index('yml')
   filename = [RAILS_ROOT, 'test', 'fixtures', filename].join('/')
-  widgets = YAML.load_file(filename)
+  YAML.load_file(filename)
+end
+
+def self.add_widgets(site, widgets)
+  unless site.network && profile = site.network.profiles.public
+    puts "ERROR: site #{site.name} does not have a network with a public profile."
+    return
+  end
+  puts "Creating widgets for site #{site.name}..."
   widgets.values.each do |params|
     puts "Creating widget #{params['name']}."
     profile.widgets.create(params)
