d130b8f8865203eaeafdf28b33e0a0af173d2eaa
k clair <kclair@pickledradish.com>
2011-04-14 11:34:51 -0400

    this is for debugging issues with IE8.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -269,6 +269,7 @@ class ApplicationController < ActionController::Base
   # which sometimes appears as :gif.
   def normal_request?
     format = request.format.to_sym
+    raise 'request format is '+format.to_s
     format = :html if (is_ie789? and format.to_s =~ /x-ms-application/)
     response.redirected_to.nil? and
     (format == :html or format == :all or format == :gif)
