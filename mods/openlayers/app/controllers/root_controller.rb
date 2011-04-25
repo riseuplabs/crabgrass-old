1fcd3dc396c3d533f6c29e8bc6018b9c5a504949
Azul <azul@riseup.net>
2011-04-25 19:12:09 +0200

    CUSTOM: switch to google street maps

--- mods/openlayers/app/controllers/root_controller.rb
+++ mods/openlayers/app/controllers/root_controller.rb
@@ -1,4 +1,6 @@
 class RootController < ApplicationController
 
   javascript 'OpenLayers', 'OpenStreetMap', :plugin => 'openlayers'
+  javascript "http://maps.google.com/maps/api/js?sensor=false"
+
 end
