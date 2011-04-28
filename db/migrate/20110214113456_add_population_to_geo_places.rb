0fdc0c5cd0a148c369122c76121e1c403dccdeff
k clair <kclair@pickledradish.com>
2011-04-28 11:57:43 -0400

    remove duplicate migration

--- db/migrate/20110214113456_add_population_to_geo_places.rb
+++
@@ -1,9 +0,0 @@
-class AddPopulationToGeoPlaces < ActiveRecord::Migration
-  def self.up
-    add_column :geo_places, :population, :bigint
-  end
-
-  def self.down
-    remove_column :geo_places, :population
-  end
-end
