316937f62e889f4fffb0775494512b05ad31a083
Azul <azul@riseup.net>
2011-05-26 23:17:34 +0200

    fixed posts controller. Do NOT use login twice in a single test
    moved test data into fixtures instead of generating in every test
    
    login_as does NOT remove the last user logged in - so you can not use this twice in the same test.
    
    I tried to fix but it's more complex than I thought and you should not do this anyway. Generate your test data ahead of time and test a single action per test to isolate the tests.

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,17 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'posts_controller'
 
-# Re-raise errors caught by the controller.
-class PostsController; def rescue_action(e) raise e end; end
-
-class PostsControllerTest < Test::Unit::TestCase
-  fixtures :pages, :users, :groups, :user_participations, :group_participations, :discussions, :memberships, :sites
-
-  def setup
-    @controller = PostsController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-  end
+class PostsControllerTest < ActionController::TestCase
+  fixtures :pages, :users, :groups, :user_participations, :group_participations, :discussions, :memberships, :sites, :posts
 
 # TODO: tests to ensure that users without access can't post
 
@@ -32,32 +22,35 @@ class PostsControllerTest < Test::Unit::TestCase
 
   def test_edit
     login_as :red
-    post :create, :post => {:body => 'test post'}, :page_id => pages(:page1).id
-    post_id = pages(:page1).discussion.posts.last.id
-
+    post_id = posts(:reds_post).id
     post :edit, :id => post_id
+
+    assert_response :success
 #    assert_template 'edit'
   end
 
   def test_save
     login_as :red
-    post :create, :post => {:body => 'test post'}, :page_id => pages(:page1).id
-    post_id = pages(:page1).discussion.posts.last.id
+    post_id = posts(:reds_post).id
 
     post :save, :id => post_id, :body => 'new test post', :save => 'Save'
-    assert pages(:page1).discussion.posts.last.body = 'new test post'
+    assert_equal 'new test post',  Post.find(post_id).body
 
     post :save, :id => post_id, :body => 'new test post', :destroy => 'Delete'
-    assert_nil pages(:page1).discussion.posts.detect { |post| post.body = 'new test post' }
+    assert_nil pages(:page1).discussion.posts.detect { |post| post.body == 'new test post' }
   end
 
-  def test_twinkle
+  def test_no_twinkle_with_same_user
+    post_id = posts(:reds_post).id
+
     login_as :red
-    post :create, :post => {:body => 'test post'}, :page_id => pages(:page1).id
-    post_id = pages(:page1).discussion.posts.last.id
     assert_no_difference 'Post.find(post_id).ratings.count' do
       post :twinkle, :id => post_id
     end
+  end
+
+  def test_twinkle
+    post_id = posts(:reds_post).id
 
     login_as :blue
     assert_difference 'Post.find(post_id).ratings.count' do
@@ -66,13 +59,11 @@ class PostsControllerTest < Test::Unit::TestCase
   end
 
   def test_untwinkle
-    login_as :red
-    post :create, :post => {:body => 'test post'}, :page_id => pages(:page1).id
-    post_id = pages(:page1).discussion.posts.last.id
+    reds_post = posts(:reds_post)
+    reds_post.ratings.create(:user_id => users(:blue).id, :rating => 1)
+    post_id = reds_post.id
 
     login_as :blue
-    post :twinkle, :id => post_id
-
     assert_difference 'Post.find(post_id).ratings.count', -1 do
       post :untwinkle, :id => post_id
     end
