5a8c1b93e3782b546eecb70c1c8d36740e541ebe
Azul <azul@riseup.net>
2011-06-20 17:43:56 +0200

    some initial gallery and asset data and params set accordingly

--- tools/gallery_tool/test/functional/gallery_controller_test.rb
+++ tools/gallery_tool/test/functional/gallery_controller_test.rb
@@ -4,15 +4,25 @@ class GalleryControllerTest < ActionController::TestCase
   fixtures :pages, :users
 
   def setup
-    # make some galleries? there are no galleries in fixtures.
+    # let's make some gallery
+    # there are no galleries in fixtures yet.
+    #
+    @gallery = Gallery.create! :title => 'gimme pictures', :user => users(:blue)
+    @asset = Asset.create_from_params({
+      :uploaded_data => upload_data('photo.jpg')}) do |asset|
+        asset.parent_page = @gallery
+      end
+    @gallery.add_image!(@asset, users(:blue))
+    @asset.save!
   end
 
 # this controller does not really even exist yet:
+  #azul: I think it does - at least there is some base page magic
   def test_create
     login_as :blue
 
     assert_difference 'Gallery.count' do
-      post :create, :id => Gallery.param_id, :page => {:title => 'pictures'}, :assets => [upload_data('photo.jpg')]
+      post :create, :page_id => Gallery.param_id, :page => {:title => 'pictures'}, :assets => [upload_data('photo.jpg')]
     end
 
     assert_not_nil assigns(:page)
@@ -21,46 +31,22 @@ class GalleryControllerTest < ActionController::TestCase
     assert_equal assigns(:page).page_terms, assigns(:page).images.first.page_terms
   end
 
-  def test_create_same_name
-    login_as :gerrard
-
-    data_ids, page_ids, page_urls = [],[],[]
-    3.times do
-      post 'create', :page => {:title => "dupe", :summary => ""}, :id => Gallery.param_id
-      page = assigns(:page)
-
-      assert_equal "dupe", page.title
-      assert_not_nil page.id
-
-      # check that we have:
-      # a new page
-      assert !page_ids.include?(page.id)
-      # a new url
-      assert !page_urls.include?(page.name_url)
-
-      # remember the values we saw
-      page_ids << page.id
-      page_urls << page.name_url
-    end
-  end
-
   def test_show
-    gallery = Gallery.find(:first)
     login_as :blue
-    get :show, :id => gallery.id
+    get :show, :page_id => @gallery.id
     assert_response :success
     assert_not_nil assigns(:images)
   end
 
   def test_edit
     login_as :blue
-    get :edit, :id => Gallery.find(:first).id
+    get :edit, :page_id => @gallery.id
     assert_response :success
   end
 
   def test_update
     login_as :blue
-    post :update, :id => Galler.find(:first).id, :title => 'New Gallery Title'
+    post :update, :page_id => Gallery.find(:first).id, :title => 'New Gallery Title'
     assert_response :success
     assert assigns(:page).title == 'New Gallery Title'
     # what else do we update for the gallery?
--- tools/gallery_tool/test/functional/gallery_image_controller_test.rb
+++ tools/gallery_tool/test/functional/gallery_image_controller_test.rb
@@ -4,7 +4,16 @@ class GalleryImageControllerTest < ActionController::TestCase
   fixtures :pages, :users
 
   def setup
-    # make some gallery images? or use fixtures. also needs galleries, maybe fixtures is the way to go.
+    # let's make some gallery
+    # there are no galleries in fixtures yet.
+    #
+    @gallery = Gallery.create! :title => 'gimme pictures', :user => users(:blue)
+    @asset = Asset.create_from_params({
+      :uploaded_data => upload_data('photo.jpg')}) do |asset|
+        asset.parent_page = @gallery
+      end
+    @gallery.add_image!(@asset, users(:blue))
+    @asset.save!
   end
 
   def test_new
@@ -15,40 +24,37 @@ class GalleryImageControllerTest < ActionController::TestCase
 
   def test_create_zip
     login_as :blue
-    post :create, :gallery_id => Gallery.find(:first).id, :file => upload_data('subdir.zip')
+    post :create, :page_id => @gallery.id, :file => upload_data('subdir.zip')
     assert_response :success  # or does this redirect to the edit page?
   end
 
   def test_create
     login_as :blue
-    post :create, :gallery_id => Gallery.find(:first).id, :file => upload_data('photo.jpg')
+    post :create, :page_id => @gallery.id, :file => upload_data('photo.jpg')
     assert_response :success # or does this redirect to the edit page?
   end
 
   def test_edit
     login_as :blue
-    asset = '' ## ??
-    get :edit, :id => asset.id
+    get :edit, :id => @asset.id
     assert_response :success
-    assert assigns(:image).id == asset.id  
+    assert assigns(:image).id == @asset.id
   end
 
   def test_update
     login_as :blue
-    asset = '' ## ??
-    post :update, :id => asset.id, :title => 'New Title'
+    post :update, :id => @asset.id, :title => 'New Title'
     assert_response :success
     assert assigns(:image).title == 'New Title'
 
-    post :update, :id => asset.id, :cover => 1
+    post :update, :id => @asset.id, :cover => 1
     assert_response :success
   end
 
   def test_destroy
     login_as :blue
-    gallery = '' ## ??
-    assert_difference Asset.count do
-      delete :destroy, :id => 1
+    assert_difference '@gallery.assets.count' do
+      delete :destroy, :id => @asset.id
     end
   end
 
