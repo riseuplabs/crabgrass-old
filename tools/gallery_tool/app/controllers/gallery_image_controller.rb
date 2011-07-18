0b314f43850153aa5a571ea3fc6e3b1f8be6eee3
K Clair <kclair@pickledradish.com>
2011-07-18 11:01:28 -0400

    edit image upload should be using the file_field partial from assets

--- tools/gallery_tool/app/controllers/gallery_image_controller.rb
+++ tools/gallery_tool/app/controllers/gallery_image_controller.rb
@@ -36,8 +36,8 @@ class GalleryImageController < BasePageController
     # whoever may edit the gallery, may edit the assets too.
     raise PermissionDenied unless current_user.may?(:edit, @page)
     @image = @page.images.find(params[:id])
-    if params[:image] and params[:image][:upload_file] #and request.xhr?
-      @image.uploaded_data = params[:image][:upload_file]
+    if params[:assets] #and request.xhr?
+      @image.uploaded_data = params[:assets].first
       if @image.save!
         @image.reload
         responds_to_parent do
