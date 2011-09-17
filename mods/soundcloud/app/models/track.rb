cd4c179ca22e2b330d95daca8887bbd50df83565
Azul <azul@riseup.net>
2011-09-17 12:08:30 +0200

    moved soundcloud stuff from controller into the track model
    Also started to enable upload and destroy

--- mods/soundcloud/app/models/track.rb
+++ mods/soundcloud/app/models/track.rb
@@ -1,15 +1,17 @@
 class Track < ActiveRecord::Base
 
+  attr_accessible :title, :asset_data
+
   validates_presence_of :permalink_url
   validates_presence_of :title
 
-  attribute_accessor :asset_data
+  attr_accessor :asset_data
 
   def self.params_from_soundcloud(remote_track)
     remote_track.slice(:permalink_url, :title, :uri, :secret_uri)
   end
 
-  before_create :create_on_soundcloud
+  before_validation :create_on_soundcloud
   def create_on_soundcloud
     soundcloud_track = client.remote.post '/tracks',
       :track => {:title => self.title,
@@ -18,6 +20,19 @@ class Track < ActiveRecord::Base
     self.permalink_url = soundcloud_track[:permalink_url]
     self.uri = soundcloud_track[:uri]
     self.secret_uri = soundcloud_track[:secret_uri]
+  end
+
+  before_update :update_on_soundcloud
+  def update_on_soundcloud
+    soundcloud_track = client.remote.put self.uri,
+      :track => {:title => self.title,
+        :sharing => 'private',
+        :asset_data => File.new(asset_data.path)}
+  end
+
+  before_destroy :destroy_on_soundcloud
+  def destroy_on_soundcloud
+    client.remote.delete self.uri
   end
 
   protected
