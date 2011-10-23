5e5e02ef0b568a082e5cfecedff48aa5ed77184f
Azul <azul@riseup.net>
2011-10-23 11:01:05 +0200

    redirect :back requires HTTP_REFERER to be set.

---
+++ test/functional/admin/sites_controller_test.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+#require 'admin/base_controller'
+
+## Re-raise errors caught by the controller.
+##class Admin::BaseController; def rescue_action(e) raise e end; end
+
+class Admin::SitesControllerTest < ActionController::TestCase
+
+  fixtures :users, :sites
+
+  def setup
+    enable_site_testing('unlimited')
+  end
+
+  def teardown
+    disable_site_testing
+  end
+
+  def test_all_profiles_visible
+    login_as :blue
+    @request.env["HTTP_REFERER"] = '/admin/sites/profile'
+    post :update, :site => {:all_profiles_visible => '1'}
+    assert_response :redirect
+    site_model = assigns('current_site')
+    assert site_model.all_profiles_visible
+  end
+
+  def test_no_admin
+    login_as :red
+    get :index
+    assert_permission_denied "only site admins may access the actions."
+  end
+
+  def test_no_site
+    disable_site_testing
+    login_as :penguin
+    get :index
+    assert_permission_denied "none of the base actions should be enabled without sites."
+  end
+
+end
